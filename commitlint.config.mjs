fd3553b15a68fdf4fe58e86b1454cdef943bed44
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-08-21 01:31:58 +0300

    build(root): adjust scope for commits

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -2,7 +2,8 @@ export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "header-max-length": [2, "always", 120],
-    "scope-enum": [2, "always", "root, public, app, widgets, shared"],
+    "scope-empty": [2, "never"],
+    "scope-enum": [2, "always", ["root", "public", "app", "widgets", "shared"]],
     "body-max-line-length": [2, "always", 200],
   },
 };
