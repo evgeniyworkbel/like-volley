95ac7d3f42cd60d39a99a6b4de3f536e1ca81e0a
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-08-21 01:12:07 +0300

    build: install eslint-config-prettier and prettier, create its config, add ignore file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
     rules: {
       "import/no-anonymous-default-export": "off",
     },
