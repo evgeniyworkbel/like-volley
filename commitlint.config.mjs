31773d6b4484797a3bfde5f988b5e27eac43a98b
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-08-21 01:26:24 +0300

    build: specify scope enums for commits

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,8 @@
-export default { extends: ["@commitlint/config-conventional"] };
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 120],
+    "scope-enum": [2, "always", "root, public, app, widgets, shared"],
+    "body-max-line-length": [2, "always", 200],
+  },
+};
