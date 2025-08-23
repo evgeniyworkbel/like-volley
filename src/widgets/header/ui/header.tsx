88d634ca600a3071ad25cfe7ca5ef99c065cb019
Ivan Khristenko <ivan.tihii1984@gmail.com>
2025-08-23 20:19:12 +0300

    fix(shared,widgets): changed of import, remove div

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,28 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
-import { navLinks } from "@/shared/constants/navigation";
+import { navLinks } from "@/shared/constants";
 
 export function Header() {
   const { home, blog, about, reviews, phone } = navLinks;
   return (
-    <header>
-      <div className="flex justify-between h-27 py-6 px-21">
-        <Image
-          src="/header.png"
-          width={89}
-          height={59}
-          alt="Логотип школы волейбола Like Volley"
-        />
-        <nav className="flex items-center gap-15 font-bold">
-          <Link href={home.href}>{home.title}</Link>
-          <Link href={blog.href}>{blog.title}</Link>
-          <Link href={about.href}>{about.title}</Link>
-          <Link href={reviews.href}>{reviews.title}</Link>
-          <Link className="text-orange-500" href={phone.href}>
-            {phone.title}
-          </Link>
-        </nav>
-      </div>
+    <header className="flex justify-between h-27 py-6 px-21">
+      <Image
+        src="/header.png"
+        width={89}
+        height={59}
+        alt="Логотип школы волейбола Like Volley"
+      />
+      <nav className="flex items-center gap-15 font-bold">
+        <Link href={home.href}>{home.title}</Link>
+        <Link href={blog.href}>{blog.title}</Link>
+        <Link href={about.href}>{about.title}</Link>
+        <Link href={reviews.href}>{reviews.title}</Link>
+        <Link className="text-orange-500" href={phone.href}>
+          {phone.title}
+        </Link>
+      </nav>
     </header>
   );
 }
