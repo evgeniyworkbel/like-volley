cd1df214f01f6097287b51f3c44736ec2f219f60
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-08-24 15:28:54 +0300

    fix(app): rename font css variables, remove shantell as fallback for body, add div wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import { Header } from "@/widgets/header";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Header />
       <main></main>
       <footer></footer>
-    </>
+    </div>
   );
 }
