b8437563d69e1e8d767ee38709cda85dd7071b43
Ivan <ivan.tihii1984@gmail.com>
2025-09-10 09:02:25 +0300

    refactor(widgets, shared): change  advantagesData file

--- src/widgets/advantages/ui/advantages.tsx
+++ src/widgets/advantages/ui/advantages.tsx
@@ -4,23 +4,15 @@ import Image from "next/image";
 export function Advantages() {
   return (
     <section className="flex flex-col gap-12 px-10 py-[74px] text-center text-sm xl:flex-row xl:justify-between xl:gap-0 xl:px-20 xl:text-base">
-      {advantagesData.map(({ title, image }) => {
+      {advantagesData.map(({ title, imageSrc, description, alt }) => {
         return (
           <div
-            key={image}
+            key={imageSrc}
             className="flex max-w-[324px] flex-col items-center gap-3 xl:gap-4"
           >
-            <Image
-              className="pb-1.5"
-              src={image}
-              width={80}
-              height={80}
-              alt="Декоративная картинка"
-            />
+            <Image src={imageSrc} width={80} height={80} alt={alt} />
             <h3 className="font-bold">{title}</h3>
-            <h4 className="text-foreground-secondary">
-              Квалифицированные тренеры и удобные локации
-            </h4>
+            <h4 className="text-foreground-secondary">{description}</h4>
           </div>
         );
       })}
