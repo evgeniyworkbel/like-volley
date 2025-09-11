c4da0bfbf6b41f53a4e55e30759792c22a221782
Ivan <ivan.tihii1984@gmail.com>
2025-09-11 16:44:02 +0300

    fix(shared): change Title.tsx remove className

--- src/shared/ui/title.tsx
+++ src/shared/ui/title.tsx
@@ -5,18 +5,15 @@ type TitleProps = Pick<
   "className" | "children"
 >;
 
-export function Title({ className, children, ...props }: TitleProps) {
+export function Title({ children, ...props }: TitleProps) {
   return (
     <h2
       {...props}
       className={cn(
         "font-shantell text-[32px] font-bold text-accent-orange xl:text-6xl",
-        className,
       )}
     >
       {children}
     </h2>
   );
 }
-
-//  className={cn("w-fit", className)}>
