6e7f54ece4b016b5e5971a9ad88625f44a0ba229
Ivan <ivan.tihii1984@gmail.com>
2025-09-15 13:45:20 +0300

    fix(widgets): add mobile footer

--- src/widgets/footer/ui/footer.tsx
+++ src/widgets/footer/ui/footer.tsx
@@ -6,36 +6,38 @@ import { contactsLinks } from "@/shared/constants";
 
 export function Footer() {
   return (
-    <footer className="grid justify-items-center gap-14 rounded-t-[20px] bg-accent-orange px-2.5 pt-20 pb-20 text-white md:grid-rows-[max-content_1fr] md:px-20 md:pt-25 lg:gap-x-15 xl:mx-0 xl:grid-cols-[auto_1fr] xl:grid-rows-1">
-      <div className="hidden md:flex md:flex-col md:gap-4">
+    <footer className="grid justify-items-center gap-15 rounded-t-[20px] bg-accent-orange py-[50px] text-white xl:mx-0 xl:grid-cols-[auto_1fr] xl:grid-rows-1 xl:gap-[8vw] xl:p-20">
+      <div className="flex flex-col items-center gap-8 xl:items-start">
         <Image
           className="bg-accent-orange"
           src="/logo.png"
-          width={51}
-          height={37}
+          width={89}
+          height={60}
           alt="Логотип школы волейбола Like Volley"
         />
-        <p className="flex flex-col gap-2 text-sm">
-          <span>Волейбол для всех возрастов</span>
-          <span>с индивидуальным подходом и</span>
-          <span>современными методиками обучения.</span>
+        <p className="flex max-w-[270px] flex-col gap-2 text-center text-lg xl:max-w-[350px] xl:text-left">
+          Волейбол для всех возрастов с индивидуальным подходом и современными
+          методиками обучения.
         </p>
       </div>
 
-      <div className="flex flex-wrap justify-center gap-[60px] xl:w-full xl:justify-between">
-        <div className="flex flex-col justify-between gap-10 md:mr-auto lg:mr-0">
+      <div className="flex flex-wrap justify-center gap-15 xl:justify-between">
+        <div className="flex flex-col items-center gap-10 xl:items-start">
           <p className="font-shantell text-[38px] uppercase">меню</p>
-          <nav className="flex items-center gap-7 text-sm md:gap-8.5 md:text-2xl">
+          <nav className="flex flex-wrap justify-center gap-7 text-2xl">
             <Link href={navLinks.home.href}>{navLinks.home.title}</Link>
             <Link href={navLinks.blog.href}>{navLinks.blog.title}</Link>
-            <Link className="text-nowrap" href={navLinks.about.href}>
-              {navLinks.about.title}
-            </Link>
+            <Link href={navLinks.about.href}>{navLinks.about.title}</Link>
             <Link href={navLinks.reviews.href}>{navLinks.reviews.title}</Link>
           </nav>
-          <div className="hidden md:flex md:gap-x-20 md:text-xs">
-            <Link href={navLinks.policy.href}>{navLinks.policy.title}</Link>
-            <Link href={navLinks.offerAgreement.href}>
+          <div className="flex justify-between gap-x-12 text-sm xl:text-xs">
+            <Link className="flex hover:underline" href={navLinks.policy.href}>
+              {navLinks.policy.title}
+            </Link>
+            <Link
+              className="flex hover:underline"
+              href={navLinks.offerAgreement.href}
+            >
               {navLinks.offerAgreement.title}
             </Link>
           </div>
@@ -46,7 +48,6 @@ export function Footer() {
               rel="noopener noreferrer"
             >
               <Image
-                className="h-15 w-15 md:h-[68px] md:w-[68px]"
                 src="/phone.svg"
                 width={68}
                 height={68}
@@ -59,7 +60,6 @@ export function Footer() {
               rel="noopener noreferrer"
             >
               <Image
-                className="h-15 w-15 md:h-[68px] md:w-[68px]"
                 src="/location.svg"
                 width={68}
                 height={68}
@@ -72,7 +72,6 @@ export function Footer() {
               rel="noopener noreferrer"
             >
               <Image
-                className="h-15 w-15 md:h-[68px] md:w-[68px]"
                 src="/email.svg"
                 width={68}
                 height={68}
@@ -85,7 +84,6 @@ export function Footer() {
               rel="noopener noreferrer"
             >
               <Image
-                className="h-15 w-15 md:h-[68px] md:w-[68px]"
                 src="/instagram.svg"
                 width={68}
                 height={68}
@@ -94,24 +92,14 @@ export function Footer() {
             </Link>
           </div>
         </div>
-        <div className="flex flex-col gap-20">
+        <div className="flex flex-col gap-20 xl:justify-between">
           <Button className="rounded-[40px] bg-white px-20 py-4 text-2xl font-medium text-black">
             Записаться
           </Button>
           <ul className="flex flex-col gap-2 text-sm">
             <li>ИП Бла бла бла</li>
             <li>НИП 0002920192381984720928573-4901</li>
-            <li className="mt-12">
-              {/* перепроверить при окончательном варианте   НА Мобильной  Версии */}
-              <div className="flex gap-x-20 text-xs">
-                <Link href={navLinks.policy.href}>{navLinks.policy.title}</Link>
-                <Link href={navLinks.offerAgreement.href}>
-                  {navLinks.offerAgreement.title}
-                </Link>
-              </div>
-              {/* перепроверить при окончательном варианте */}
-            </li>
-            <li className="mb-4">ИНН 2039847948</li>
+            <li className="mb-5">ИНН 2039847948</li>
             <li>© 2024 LikeVolley Все права защищены.</li>
           </ul>
         </div>
