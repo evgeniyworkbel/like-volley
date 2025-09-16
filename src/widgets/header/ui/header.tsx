65bc90280601dfacf04061725f64983753270558
Ivan <ivan.tihii1984@gmail.com>
2025-09-16 14:35:46 +0300

    fix(widgets): rename BurgerMenu.tsx

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,7 +1,7 @@
 import { navLinks } from "@/shared/constants";
 import Image from "next/image";
 import Link from "next/link";
-import { BurgerMenu } from "./ burger-menu";
+import { BurgerMenu } from "./burger-menu";
 
 export function Header() {
   return (
