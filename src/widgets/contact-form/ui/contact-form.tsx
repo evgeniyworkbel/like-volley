3e0a45e918e1829a4886c903f275af91e11b16e1
Ivan Khristenko <ivan.tihii1984@gmail.com>
2025-09-21 10:59:54 +0300

    refactor(widgets): change Contact.tsx

--- src/widgets/contact-form/ui/contact-form.tsx
+++ src/widgets/contact-form/ui/contact-form.tsx
@@ -1,24 +1,22 @@
 import { Title } from "@/shared/ui";
 import { Contact } from "./contact";
 import { Form } from "./form";
-import { contacts } from "../model/contacts";
+import { contactsData } from "../model/data";
 
 export function ContactForm() {
   return (
     <section className="flex flex-col gap-12 rounded-[20px] bg-accent-orange-muted px-5 py-10 xl:flex-row xl:justify-between xl:px-20 xl:py-[90px]">
       <div className="flex flex-col gap-10 xl:max-w-[494px] xl:gap-12">
         <div className="flex flex-col gap-4 text-center xl:pt-28">
-          <Title className="inline text-[28px] xl:block xl:text-left xl:text-6xl">
+          <Title className="text-center xl:text-left">
             Запишись&nbsp;
-            <span className="text-foreground xl:block">
-              на первую тренировку
-            </span>
+            <span className="text-foreground">на первую тренировку</span>
           </Title>
           <p className="text-base text-foreground-secondary">
             Свяжитесь с нами для получения персональной консультации
           </p>
         </div>
-        {contacts.map(({ title, imageSrc, alt, href }) => (
+        {contactsData.map(({ title, imageSrc, alt, href }) => (
           <Contact
             title={title}
             imageSrc={imageSrc}
--- src/widgets/contact-form/ui/contact.tsx
+++ src/widgets/contact-form/ui/contact.tsx
@@ -11,15 +11,18 @@ type ContactProps = {
 
 export function Contact({ title, imageSrc, alt, href }: ContactProps) {
   return (
-    <div className="flex flex-col justify-between gap-6">
-      <div className="flex gap-6 xl:gap-7" key={title}>
-        <Image src={imageSrc} width={48} height={48} alt={alt} />
-        <div className="flex flex-col">
-          <h3 className="font-bold">{title}</h3>
-          <Link href={href} target="_blank" rel="noopener noreferrer">
-            <span className="text-foreground-secondary">{href}</span>
-          </Link>
-        </div>
+    <div className="flex gap-6 xl:gap-7">
+      <Image src={imageSrc} width={48} height={48} alt={alt} />
+      <div className="flex flex-col">
+        <h3 className="font-bold">{title}</h3>
+        <Link
+          className="text-foreground-secondary"
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {href}
+        </Link>
       </div>
     </div>
   );
