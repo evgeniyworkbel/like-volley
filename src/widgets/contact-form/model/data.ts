d1feea681464d0e9f3d288b726582064c73b4462
Ivan Khristenko <ivan.tihii1984@gmail.com>
2025-09-21 15:56:07 +0300

    refactor(widgets): change of Adwantage, Contact

--- src/widgets/contact-form/model/data.ts
+++ src/widgets/contact-form/model/data.ts
@@ -1,23 +1,25 @@
+import { contactsLinks } from "@/shared/constants";
+
 export const contactsData = [
   {
     title: "Телефон",
     href: "tel:+375298020748",
     imageSrc: "/contact-form/phone.svg",
     alt: "Декоративная иконка телефона школы волейбола Like Volley",
-    label: "+375298020748",
+    label: contactsLinks.phone.href,
   },
   {
     title: "Telegram",
     href: "https://www.telegram.com/like",
     imageSrc: "/contact-form/telegram.svg",
     alt: "Декоративная иконка Telegram школы волейбола Like Volley",
-    label: "@LikeVolleyBrest",
+    label: contactsLinks.telegram.href,
   },
   {
     title: "Instagram",
     href: "https://www.instagram.com/like",
     imageSrc: "/contact-form/instagram.svg",
     alt: "Декоративная иконка Instagram школы волейбола Like Volley",
-    label: "@LikeVolleyBrest",
+    label: contactsLinks.instagram.href,
   },
 ] as const;
