8d34c9948f028a5bb25f0df39f21549a4b6a6ee3
Ivan <ivan.tihii1984@gmail.com>
2025-09-26 16:12:51 +0300

    refactor(shared): change  CoachCard

--- src/shared/ui/carousel/coaches-сard.tsx
+++ src/shared/ui/carousel/coaches-сard.tsx
@@ -4,7 +4,9 @@ import { Coach } from "./model/types";
 export function CoachCard({ name, description, src, alt }: Coach) {
   return (
     <hgroup className="flex flex-col items-center gap-5">
-      <Image src={src} width={268} height={268} alt={alt} />
+      <div className="relative h-[268px] w-[268px] overflow-hidden rounded-[20px] xl:border-4 xl:border-accent-blue">
+        <Image className="object-cover object-top" src={src} alt={alt} fill />
+      </div>
       <h4 className="flex flex-col items-center gap-3">
         <p className="text-xl font-bold text-accent-orange">{name}</p>
         <span className="text-center text-sm text-foreground-secondary">
--- src/widgets/coaches/model/data.ts
+++ src/widgets/coaches/model/data.ts
@@ -3,25 +3,25 @@ export const coachesData: Coach[] = [
   {
     name: "Евгений Быков",
     description: "Старший тренер в городе Бресте, чемпион по волейболу 2018г",
-    src: "/coaches/bulls.png",
+    src: "/coaches/bulls.jpg",
     alt: "Фото тренера Евгения Быкова",
   },
   {
     name: "Александр Рыбко",
     description: "Старший тренер в городе Бресте, чемпион по волейболу 2018г",
-    src: "/coaches/rybko.png",
+    src: "/coaches/rybko.jpg",
     alt: "Фото тренера Александра Рыбко",
   },
   {
     name: "Игорь Гурский",
     description: "Старший тренер в городе Бресте, чемпион по волейболу 2018г",
-    src: "/coaches/gursky.png",
+    src: "/coaches/gursky.jpg",
     alt: "Фото тренера Игоря Гурского",
   },
   {
     name: "Ангелина Горбачева",
     description: "Старший тренер в городе Бресте, чемпион по волейболу 2018г",
-    src: "/coaches/gorbachev's.png",
+    src: "/coaches/gorbachev's.jpg",
     alt: "Фото тренера Ангелины Горбачевой",
   },
 ] as const;
