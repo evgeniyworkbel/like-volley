b8c5e54d90ec428b6456154787e05264904f4370
Ivan Khristenko <ivan.tihii1984@gmail.com>
2025-09-27 20:19:58 +0300

    fix(shared): add frame mobile 4px

--- src/shared/ui/carousel/coaches-сard.tsx
+++ src/shared/ui/carousel/coaches-сard.tsx
@@ -4,7 +4,7 @@ import { Coach } from "./model/types";
 export function CoachCard({ name, description, src, alt }: Coach) {
   return (
     <hgroup className="flex flex-col items-center gap-5">
-      <div className="relative h-[268px] w-[268px] overflow-hidden rounded-[20px] xl:border-4 xl:border-accent-blue">
+      <div className="relative h-[268px] w-[268px] overflow-hidden rounded-[20px] border-4 border-accent-blue">
         <Image className="object-cover object-top" src={src} alt={alt} fill />
       </div>
       <h4 className="flex flex-col items-center gap-3">
