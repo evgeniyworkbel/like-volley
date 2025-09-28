73658b0d8fdc8cc5fc98661ce2ebd9500694e132
Ivan Khristenko <ivan.tihii1984@gmail.com>
2025-09-28 08:29:32 +0300

    refactor(app): rename font, cahnge css style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <section className="flex flex-col items-center gap-12 px-[24px] pt-30 pb-[170px] xl:px-115">
-      <Title className="text-[74px] text-foreground xl:text-9xl">
+    <section className="relative flex flex-col items-center gap-12 px-[24px] pt-30 pb-[170px] xl:px-115">
+      <h1 className="font-shantell text-[74px] text-foreground xl:text-9xl">
         4<span className="text-accent-orange">0</span>4
-      </Title>
-      <h3 className="font-shantell text-2xl font-bold xl:text-[40px]">
+      </h1>
+      <Title className="text-2xl text-foreground xl:text-[40px]">
         Страница не найдена :(
-      </h3>
-      <div className="relative flex h-[226px] w-full xl:h-[380px] xl:w-[518px]">
-        <Image fill src="/not-found/boll.png" alt="Декоративная картинка мяч" />
-      </div>
+      </Title>
+      <Image src="/not-found/boll.png" alt="" fill />
     </section>
   );
 }
+
+/* <div className="relative flex h-[226px] w-full xl:h-[380px] xl:w-[518px]"></div> */
