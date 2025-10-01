418e69d46b3a5a24cc3220a48b9fb08f66455b43
Ivan <ivan.tihii1984@gmail.com>
2025-10-01 12:43:07 +0300

    refactor(shared): change onClick event

--- src/shared/ui/accordion.tsx
+++ src/shared/ui/accordion.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { AccordionArrowIcon } from "../icons";
 
 type AccordionProps = {
@@ -10,31 +9,31 @@ type AccordionProps = {
   }[];
 };
 
+const handleClick = ({
+  currentTarget,
+}: React.MouseEvent<HTMLDetailsElement>) => {
+  document.querySelectorAll("details").forEach((detail) => {
+    if (detail !== currentTarget) {
+      detail.removeAttribute("open");
+    }
+  });
+};
+
 export function Accordion({ items }: AccordionProps) {
-  const [openIndex, setOpenIndex] = useState<number | null>(null);
-
-  const handleToggle = (index: number) => (event: React.MouseEvent) => {
-    event.preventDefault();
-    setOpenIndex(openIndex === index ? null : index);
-  };
-
   return (
     <div className="flex flex-col gap-5">
-      {items.map(({ question, answer }, index) => {
-        const isOpen = openIndex === index;
-
+      {items.map(({ question, answer }) => {
         return (
           <details
             key={question}
-            open={isOpen}
             className="group cursor-pointer rounded-xl border border-accent-orange-light hover:border-accent-orange"
-            onClick={handleToggle(index)}
+            onClick={handleClick}
           >
             <summary className="mb-2 flex list-none items-center justify-between p-6 text-lg font-semibold select-none">
               {question}
               <AccordionArrowIcon className="group-open:rotate-180 [&>:first-child]:fill-accent-orange-light [&>:first-child]:hover:fill-accent-orange" />
             </summary>
-            {isOpen && <div className="px-6 py-3">{answer}</div>}
+            <div className="px-6 py-3">{answer}</div>
           </details>
         );
       })}
