18476d7a107debb9ca3c67708107535a3989712d
Ivan <ivan.tihii1984@gmail.com>
2025-10-01 11:11:59 +0300

    feat(widgets): add input

--- src/widgets/contact-form/ui/form.tsx
+++ src/widgets/contact-form/ui/form.tsx
@@ -1,12 +1,45 @@
+"use client";
+
 import { Button } from "@/shared/ui";
+import { useForm } from "react-hook-form";
 
 export function Form() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      name: "",
+      phone: "",
+    },
+  });
+
+  console.log(watch("name"));
+
   return (
     <div className="flex w-full flex-col items-center gap-6 rounded-3xl bg-accent-blue p-6">
-      <div className="flex h-[600px]"></div>
-      <Button className="rounded-[40px] bg-white px-10 py-4 text-lg xl:px-52">
-        Отправить сообщение
-      </Button>
+      <form
+        className="flex flex-col text-base text-white [&_>input]:h-10 [&_>input]:w-full [&_>input]:rounded-lg [&_>input]:bg-white"
+        onSubmit={handleSubmit((data) => {
+          alert(JSON.stringify(data));
+        })}
+      >
+        <label>Имя</label>
+        <input {...register("name")} defaultValue="test" />
+
+        <label>Телефон</label>
+        <input
+          className="text-foreground-secondary"
+          placeholder="+375 (__) ___-__-__"
+          {...register("phone", { required: true, maxLength: 10 })}
+        />
+
+        <Button className="rounded-[40px] bg-white px-10 py-4 text-lg xl:px-52">
+          Отправить сообщение
+        </Button>
+      </form>
     </div>
   );
 }
