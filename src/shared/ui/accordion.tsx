a6a2b08c5eca2be93dd345e6ac2dd6e447ff39e9
Ivan <ivan.tihii1984@gmail.com>
2025-10-01 14:35:45 +0300

    refactor(shared): change Accordion

--- src/shared/ui/accordion.tsx
+++ src/shared/ui/accordion.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { AccordionArrowIcon } from "../icons";
 
 type AccordionProps = {
@@ -9,25 +7,15 @@ type AccordionProps = {
   }[];
 };
 
-const handleClick = ({
-  currentTarget,
-}: React.MouseEvent<HTMLDetailsElement>) => {
-  document.querySelectorAll("details").forEach((detail) => {
-    if (detail !== currentTarget) {
-      detail.removeAttribute("open");
-    }
-  });
-};
-
 export function Accordion({ items }: AccordionProps) {
   return (
     <div className="flex flex-col gap-5">
       {items.map(({ question, answer }) => {
         return (
           <details
+            name="requirements"
             key={question}
             className="group cursor-pointer rounded-xl border border-accent-orange-light hover:border-accent-orange"
-            onClick={handleClick}
           >
             <summary className="mb-2 flex list-none items-center justify-between p-6 text-lg font-semibold select-none">
               {question}
