c646de4a71938bc676a3d9375147d7b097e7bff5
Ivan <ivan.tihii1984@gmail.com>
2025-10-03 16:14:48 +0300

    fix(app): change gif

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,18 +5,18 @@ export default function NotFound() {
   return (
     <div>
       <Header />
-      <div className="flex flex-col items-center gap-6 px-[24px] pt-30 pb-[170px] xl:gap-12 xl:px-12 xl:pb-24">
+      <main className="flex flex-col items-center gap-6 px-[24px] pt-30 pb-[170px] font-bold xl:gap-12 xl:px-12 xl:pb-24">
         <h1 className="font-shantell text-[74px] text-foreground xl:text-9xl">
           4<span className="text-accent-orange">0</span>4
         </h1>
         <Title className="text-2xl text-foreground xl:text-[40px]">
           Страница не найдена :(
         </Title>
-        <video autoPlay loop muted playsInline>
-          <source src="/not-found/like-volley.webm" />
-          <source src="/not-found/like volley.mp4" />
+        <video autoPlay muted playsInline>
+          <source src="/not-found/animated-ball.webm" />
+          <source src="/not-found/animated-ball.mp4" />
         </video>
-      </div>
+      </main>
       <Footer />
     </div>
   );
