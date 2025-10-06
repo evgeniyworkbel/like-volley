09a0500dedd2ad23e6ca6f3eb1065c48b1d16318
Ivan <ivan.tihii1984@gmail.com>
2025-10-06 14:20:40 +0300

    refactor(widgets): rename items

--- src/widgets/gallery/ui/gallery.tsx
+++ src/widgets/gallery/ui/gallery.tsx
@@ -7,8 +7,8 @@ export function Gallery() {
     <section className="flex flex-col gap-6 px-5 py-10">
       <Title className="text-center">Галерея</Title>
       <Carousel countSlides={3}>
-        {galleryData.map((coach) => (
-          <GalleryCard key={coach.alt} src={coach.src} alt={coach.alt} />
+        {galleryData.map((promo) => (
+          <GalleryCard key={promo.alt} src={promo.src} alt={promo.alt} />
         ))}
       </Carousel>
     </section>
