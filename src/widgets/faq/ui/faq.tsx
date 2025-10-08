d43f4d2a09355e2438c2b7edb91a4a02754bfb5e
Ivan <ivan.tihii1984@gmail.com>
2025-10-08 10:58:13 +0300

    fix(widgets): change font-size, gap mobile

--- src/widgets/faq/ui/faq.tsx
+++ src/widgets/faq/ui/faq.tsx
@@ -3,12 +3,12 @@ import { faqData } from "../model/meta";
 
 export function Faq() {
   return (
-    <div className="flex flex-col gap-32 px-5 py-10 xl:px-20">
-      <hgroup className="flex flex-col items-center gap-4 text-lg">
+    <div className="flex flex-col gap-5 px-5 py-10 xl:gap-32 xl:px-20">
+      <hgroup className="flex flex-col items-center gap-4 text-base xl:text-lg">
         <Title>
           Вопросы<span className="text-accent-orange"> и ответы</span>
         </Title>
-        <p className="flex flex-col items-center text-foreground-secondary">
+        <p className="flex flex-col items-center text-center text-foreground-secondary">
           <span>
             Выберите формат обучения, который лучше всего соответствует вашим
             предпочтениям, расписанию и бюджету.
