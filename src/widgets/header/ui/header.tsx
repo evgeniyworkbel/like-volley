58ba6d9cb6db5d6cfac8c0aa11355f43604202ab
Ivan <ivan.tihii1984@gmail.com>
2025-10-21 08:08:25 +0300

    fix(widgets): add xl:hidden for MobileMenu

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,22 +1,8 @@
-"use client";
-
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import { BurgerMenu } from "./burger-menu";
 import { MobileMenu } from "./mobile-menu";
 import { Navbar } from "./navbar";
 
 export function Header() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (isOpen) {
-      document.body.classList.add("overflow-hidden");
-    } else {
-      document.body.classList.remove("overflow-hidden");
-    }
-  }, [isOpen]);
-
   return (
     <header className="flex items-center justify-between py-6 pr-12 pl-6 text-accent-orange xl:px-20">
       <Image
@@ -26,8 +12,7 @@ export function Header() {
         alt="Логотип школы волейбола Like Volley"
       />
       <Navbar />
-      {!isOpen && <BurgerMenu onClick={() => setIsOpen(!isOpen)} />}
-      {isOpen && <MobileMenu onClick={() => setIsOpen(!isOpen)} />}
+      <MobileMenu />
     </header>
   );
 }
--- src/widgets/header/ui/mobile-menu.tsx
+++ src/widgets/header/ui/mobile-menu.tsx
@@ -1,12 +1,30 @@
-import { CloseMenu } from "./close-menu";
-import { ButtonProps } from "@/shared/ui/button";
-import { Navbar } from "./navbar";
+"use client";
 
-export function MobileMenu({ onClick }: ButtonProps) {
+import { CloseMenu } from "./close-menu";
+import { Navbar } from "./navbar";
+import { useState, useEffect } from "react";
+import { BurgerMenu } from "./burger-menu";
+
+export function MobileMenu() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [isOpen]);
+
   return (
-    <div>
-      <CloseMenu onClick={onClick} />
-      <Navbar className="fixed top-40 left-0 flex w-full flex-col rounded-b-2xl bg-[oklch(1_0_0_/_60%)] pb-5 font-shantell text-2xl backdrop-blur-sm transition-all duration-900 ease-in-out xl:hidden [&>*:last-child]:rounded-[40px] [&>*:last-child]:bg-accent-blue [&>*:last-child]:px-12 [&>*:last-child]:py-4 [&>*:last-child]:text-white [&>*:last-child]:no-underline" />
+    <div className="xl:hidden">
+      {!isOpen && <BurgerMenu onClick={() => setIsOpen(!isOpen)} />}
+      {isOpen && (
+        <div className="xl:hidden">
+          <CloseMenu onClick={() => setIsOpen(!isOpen)} />
+          <Navbar className="fixed top-40 left-0 flex w-full flex-col rounded-b-2xl bg-[oklch(1_0_0_/_60%)] pb-5 font-shantell text-2xl backdrop-blur-sm transition-all duration-900 ease-in-out [&>*:last-child]:rounded-[40px] [&>*:last-child]:bg-accent-blue [&>*:last-child]:px-12 [&>*:last-child]:py-4 [&>*:last-child]:text-white [&>*:last-child]:no-underline" />
+        </div>
+      )}
     </div>
   );
 }
