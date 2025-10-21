0206f8055795de2bed85b67af73657936383429a
Ivan <ivan.tihii1984@gmail.com>
2025-10-21 14:49:02 +0300

    refactor(widgets): change MobileMenu rename burger-menu, close-menu

--- src/widgets/header/ui/mobile-menu.tsx
+++ src/widgets/header/ui/mobile-menu.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { CloseMenu } from "./close-menu";
-import { Navbar } from "./navbar";
 import { useState, useEffect } from "react";
-import { BurgerMenu } from "./burger-menu";
+import { CloseMenuIcon } from "./close-menu-icon";
+import { Navbar } from "./navbar";
+import { BurgerMenuIcon } from "./ burger-menu-icon";
+import { Button } from "@/shared/ui";
 
 export function MobileMenu() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -16,12 +17,21 @@ export function MobileMenu() {
     }
   }, [isOpen]);
 
+  const openMenu = () => setIsOpen(true);
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <div className="xl:hidden">
-      {!isOpen && <BurgerMenu onClick={() => setIsOpen(!isOpen)} />}
+      {!isOpen && (
+        <Button onClick={openMenu}>
+          <BurgerMenuIcon />
+        </Button>
+      )}
       {isOpen && (
         <div className="xl:hidden">
-          <CloseMenu onClick={() => setIsOpen(!isOpen)} />
+          <Button onClick={closeMenu}>
+            <CloseMenuIcon />
+          </Button>
           <Navbar className="fixed top-40 left-0 flex w-full flex-col rounded-b-2xl bg-[oklch(1_0_0_/_60%)] pb-5 font-shantell text-2xl backdrop-blur-sm transition-all duration-900 ease-in-out [&>*:last-child]:rounded-[40px] [&>*:last-child]:bg-accent-blue [&>*:last-child]:px-12 [&>*:last-child]:py-4 [&>*:last-child]:text-white [&>*:last-child]:no-underline" />
         </div>
       )}
