e4e1d48f1a0d699389abc96735316d1d625c8b70
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-10-22 20:27:20 +0300

    fix(public, shared): remove script from public, return nextjs Script component with dangerouslySetInnerHTML

--- src/shared/analytics/yandex-metrika/script.tsx
+++ src/shared/analytics/yandex-metrika/script.tsx
@@ -7,26 +7,26 @@ import { YM_COUNTER_ID } from "./config";
 
 export const YandexMetrikaScript = () => {
   const pathname = usePathname();
+
   useEffect(() => {
     // @ts-expect-error "ym" is global function which is created by Yandex metrica script.
     // To leran more, see sources of external lib https://github.com/v-doronin/next-yandex-metrica/blob/main/src/lib/ym.ts
     window.ym(YM_COUNTER_ID, "hit", pathname);
   }, [pathname]);
 
-  return <script id="test" src="/metrika.js" async />;
-  // return (
-  //   <Script
-  //     id="yandex-metrika-analytics"
-  //     dangerouslySetInnerHTML={{
-  //       __html: `(function(m,e,t,r,i,k,a){
-  //         m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-  //         m[i].l=1*new Date();
-  //         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-  //         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-  //       })(window, document,'script','https://mc.webvisor.org/metrika/tag_ww.js?id=${YM_COUNTER_ID}', 'ym');
+  return (
+    <Script
+      id="yandex-metrika-analytics"
+      dangerouslySetInnerHTML={{
+        __html: `(function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.webvisor.org/metrika/tag_ww.js?id=${YM_COUNTER_ID}', 'ym');
 
-  //       ym(${YM_COUNTER_ID}, 'init', {ssr:true, webvisor:true, trackHash:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`,
-  //     }}
-  //   />
-  // );
+        ym(${YM_COUNTER_ID}, 'init', {ssr:true, webvisor:true, trackHash:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`,
+      }}
+    />
+  );
 };
