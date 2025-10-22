e3bfd142010edaeb9644acb89a64d847b7c9f631
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-10-22 20:11:26 +0300

    fix(public, app): place yandex metrika script into file, load script from file

--- src/shared/analytics/yandex-metrika/script.tsx
+++ src/shared/analytics/yandex-metrika/script.tsx
@@ -13,19 +13,20 @@ export const YandexMetrikaScript = () => {
     window.ym(YM_COUNTER_ID, "hit", pathname);
   }, [pathname]);
 
-  return (
-    <Script
-      id="yandex-metrika-analytics"
-      dangerouslySetInnerHTML={{
-        __html: `(function(m,e,t,r,i,k,a){
-          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-        })(window, document,'script','https://mc.webvisor.org/metrika/tag_ww.js?id=${YM_COUNTER_ID}', 'ym');
+  return <Script src="/metrika.js" />;
+  // return (
+  //   <Script
+  //     id="yandex-metrika-analytics"
+  //     dangerouslySetInnerHTML={{
+  //       __html: `(function(m,e,t,r,i,k,a){
+  //         m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  //         m[i].l=1*new Date();
+  //         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  //         k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+  //       })(window, document,'script','https://mc.webvisor.org/metrika/tag_ww.js?id=${YM_COUNTER_ID}', 'ym');
 
-        ym(${YM_COUNTER_ID}, 'init', {ssr:true, webvisor:true, trackHash:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`,
-      }}
-    />
-  );
+  //       ym(${YM_COUNTER_ID}, 'init', {ssr:true, webvisor:true, trackHash:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});`,
+  //     }}
+  //   />
+  // );
 };
