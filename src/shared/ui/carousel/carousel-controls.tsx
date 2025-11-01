586ce4e0c1012e281c8385e4c1fb82ef96385615
Ivan <ivan.tihii1984@gmail.com>
2025-11-01 13:05:23 +0300

    refactor(shared, widgets): change gallery

--- src/shared/ui/carousel/carousel-controls.tsx
+++ src/shared/ui/carousel/carousel-controls.tsx
@@ -3,19 +3,32 @@ import { EmblaCarouselType } from "embla-carousel";
 import { Button } from "@/shared/ui";
 import { usePrevNextButtons } from "./use-prev-next-buttons";
 
-export type CarouselControlsProps = {
+export type CarouselControlsProps = Pick<
+  React.HTMLAttributes<HTMLDivElement>,
+  "className" | "children"
+> & {
   emblaApi?: EmblaCarouselType;
 };
 
-export function CarouselControls({ emblaApi }: CarouselControlsProps) {
+export function CarouselControls({
+  emblaApi,
+  children,
+}: CarouselControlsProps) {
   const { onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi);
 
   return (
-    <div className="flex items-center justify-between xl:justify-center xl:gap-30">
-      <Button onClick={onPrevButtonClick}>
+    <div className="relative flex items-center justify-between xl:justify-center xl:gap-30">
+      <Button
+        className="absolute bottom-0 left-0 z-50 xl:top-1/2 xl:-translate-y-1/2"
+        onClick={onPrevButtonClick}
+      >
         <CarouselArrowIcon className="rotate-180" />
       </Button>
-      <Button onClick={onNextButtonClick}>
+      {children}
+      <Button
+        className="absolute right-0 bottom-0 z-50 xl:top-1/2 xl:-translate-y-1/2"
+        onClick={onNextButtonClick}
+      >
         <CarouselArrowIcon />
       </Button>
     </div>
