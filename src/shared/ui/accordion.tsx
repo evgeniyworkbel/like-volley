4411d224069ac872ca46ec614d386e086752cd2a
Ivan <ivan.tihii1984@gmail.com>
2025-11-03 17:11:12 +0300

    refactor(shared): change Accordion

--- src/shared/ui/accordion.tsx
+++ src/shared/ui/accordion.tsx
@@ -12,17 +12,17 @@ export function Accordion({ items }: AccordionProps) {
     <div className="flex flex-col gap-5">
       {items.map(({ question, answer }) => {
         return (
-          <details
-            name="faq"
+          <div
+            className="group mb-2 grid cursor-pointer list-none grid-rows-[auto_0fr] items-center justify-between rounded-xl border border-accent-orange-light p-6 text-sm font-semibold select-none hover:border-accent-orange xl:text-lg"
             key={question}
-            className="group cursor-pointer rounded-xl border border-accent-orange-light hover:border-accent-orange"
           >
-            <summary className="mb-2 flex list-none items-center justify-between p-6 text-sm font-semibold select-none xl:text-lg">
+            <div className="flex items-center justify-between overflow-hidden">
               {question}
               <AccordionArrowIcon className="group-open:rotate-180 [&>:first-child]:fill-accent-orange-light [&>:first-child]:hover:fill-accent-orange" />
-            </summary>
-            <div className="px-6 py-3">{answer}</div>
-          </details>
+            </div>
+
+            <div className="">{answer}</div>
+          </div>
         );
       })}
     </div>
