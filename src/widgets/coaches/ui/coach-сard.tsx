009bfd9fdcbc776d0f26f0e00ca5e59c73aaaade
Ivan <ivan.tihii1984@gmail.com>
2025-11-04 16:49:59 +0300

    feat(widgets): add cage for coach

--- src/widgets/coaches/ui/coach-сard.tsx
+++ src/widgets/coaches/ui/coach-сard.tsx
@@ -6,15 +6,17 @@ type CoachCardProps = Coach;
 export function CoachCard({ name, description, src, alt }: CoachCardProps) {
   return (
     <div className="flex flex-col items-center gap-5">
-      <div className="relative h-[270px] w-[270px] overflow-hidden rounded-[20px] border-4 border-accent-blue">
-        <Image src={src} alt={alt} fill />
+      <div className="rounded-[20px] border-2 border-[oklch(83.27%_0.002_247.32deg)]">
+        <div className="relative h-[270px] w-[270px] overflow-hidden rounded-t-[20px]">
+          <Image src={src} alt={alt} fill />
+        </div>
+        <hgroup className="flex max-w-[268px] flex-col items-center gap-3">
+          <h4 className="text-xl font-bold text-accent-orange">{name}</h4>
+          <p className="text-center text-sm text-foreground-secondary">
+            {description}
+          </p>
+        </hgroup>
       </div>
-      <hgroup className="flex max-w-72 flex-col items-center gap-3">
-        <h4 className="text-xl font-bold text-accent-orange">{name}</h4>
-        <p className="text-center text-sm text-foreground-secondary">
-          {description}
-        </p>
-      </hgroup>
     </div>
   );
 }
