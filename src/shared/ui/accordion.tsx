5dfcec9ca8b8e9db52ae61313369de1c5cc82846
Ivan <ivan.tihii1984@gmail.com>
2025-11-04 20:40:52 +0300

    refactor(shared): change accordion

--- src/shared/ui/accordion.tsx
+++ src/shared/ui/accordion.tsx
@@ -14,14 +14,14 @@ export function Accordion({ items }: AccordionProps) {
         return (
           <div
             key={question}
-            className="group mb-2 cursor-pointer list-none items-center justify-between rounded-xl border border-accent-orange-light p-6 text-sm font-semibold hover:border-accent-orange xl:text-lg"
+            className="group mb-2 cursor-pointer list-none items-center justify-between rounded-xl border border-accent-orange-light p-6 text-sm hover:border-accent-orange xl:text-lg"
           >
-            <div className="flex items-center justify-between select-none">
-              <div>{question}</div>
+            <div className="flex items-center justify-between font-semibold select-none">
+              {question}
               <AccordionArrowIcon className="transition-transform group-hover:rotate-180 [&>:first-child]:fill-accent-orange-light [&>:first-child]:group-hover:fill-accent-orange" />
             </div>
 
-            <div className="grid grid-rows-[0fr] transition-all duration-400 group-hover:grid-rows-[1fr]">
+            <div className="grid grid-rows-[0fr] transition-[grid-template-rows] duration-400 group-hover:grid-rows-[1fr]">
               <div className="overflow-hidden">{answer}</div>
             </div>
           </div>
