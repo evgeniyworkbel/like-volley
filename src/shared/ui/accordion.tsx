7350a763fbbdfd14e23cafdc7774b1aedcf75823
Ivan <ivan.tihii1984@gmail.com>
2025-11-04 15:52:10 +0300

    refactor(shared): change accordion

--- src/shared/ui/accordion.tsx
+++ src/shared/ui/accordion.tsx
@@ -13,10 +13,10 @@ export function Accordion({ items }: AccordionProps) {
       {items.map(({ question, answer }) => {
         return (
           <div
-            className="group mb-2 cursor-pointer list-none items-center justify-between rounded-xl border border-accent-orange-light p-6 text-sm font-semibold select-none hover:border-accent-orange xl:text-lg"
             key={question}
+            className="group mb-2 cursor-pointer list-none items-center justify-between rounded-xl border border-accent-orange-light p-6 text-sm font-semibold hover:border-accent-orange xl:text-lg"
           >
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between select-none">
               <div>{question}</div>
               <AccordionArrowIcon className="transition-transform group-hover:rotate-180 [&>:first-child]:fill-accent-orange-light [&>:first-child]:group-hover:fill-accent-orange" />
             </div>
