52930826b0c44e84558266fce01cec44885eddce
Ivan <ivan.tihii1984@gmail.com>
2025-11-04 10:46:12 +0300

    fix(shared): add animation for faq

--- src/shared/ui/accordion.tsx
+++ src/shared/ui/accordion.tsx
@@ -13,15 +13,17 @@ export function Accordion({ items }: AccordionProps) {
       {items.map(({ question, answer }) => {
         return (
           <div
-            className="group mb-2 grid cursor-pointer list-none grid-rows-[auto_0fr] items-center justify-between rounded-xl border border-accent-orange-light p-6 text-sm font-semibold select-none hover:border-accent-orange xl:text-lg"
+            className="group mb-2 cursor-pointer list-none items-center justify-between rounded-xl border border-accent-orange-light p-6 text-sm font-semibold select-none hover:border-accent-orange xl:text-lg"
             key={question}
           >
-            <div className="flex items-center justify-between overflow-hidden">
-              {question}
-              <AccordionArrowIcon className="group-open:rotate-180 [&>:first-child]:fill-accent-orange-light [&>:first-child]:hover:fill-accent-orange" />
+            <div className="flex items-center justify-between">
+              <div>{question}</div>
+              <AccordionArrowIcon className="transition-transform group-hover:rotate-180 [&>:first-child]:fill-accent-orange-light [&>:first-child]:group-hover:fill-accent-orange" />
             </div>
 
-            <div className="">{answer}</div>
+            <div className="grid grid-rows-[0fr] transition-all duration-400 group-hover:grid-rows-[1fr]">
+              <div className="overflow-hidden">{answer}</div>
+            </div>
           </div>
         );
       })}
