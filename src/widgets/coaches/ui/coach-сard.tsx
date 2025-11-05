b816ef253238bc1deb2565b7e9f0153310f08546
Ivan <ivan.tihii1984@gmail.com>
2025-11-05 17:00:19 +0300

    refactor(widgets): change coaches frame

--- src/widgets/coaches/ui/coach-сard.tsx
+++ src/widgets/coaches/ui/coach-сard.tsx
@@ -5,16 +5,18 @@ type CoachCardProps = Coach;
 
 export function CoachCard({ name, description, src, alt }: CoachCardProps) {
   return (
-    <div className="flex w-min flex-col items-center gap-5 rounded-[18px] border-2 border-[oklch(83.27%_0.002_247.32deg)]">
-      <div className="relative h-70 w-80 overflow-hidden rounded-t-[18px]">
-        <Image src={src} alt={alt} sizes="320px" fill />
+    <div className="flex flex-col items-center gap-5">
+      <div className="rounded-[18px] border-2 border-[oklch(83.27%_0.002_247.32deg)]">
+        <div className="relative h-70 w-80 overflow-hidden rounded-t-[18px]">
+          <Image src={src} alt={alt} sizes="320px" fill />
+        </div>
+        <hgroup className="flex max-w-[268px] flex-col items-center gap-3">
+          <h4 className="text-xl font-bold text-accent-orange">{name}</h4>
+          <p className="text-center text-sm text-foreground-secondary">
+            {description}
+          </p>
+        </hgroup>
       </div>
-      <hgroup className="flex max-w-[268px] flex-col items-center gap-3">
-        <h4 className="text-xl font-bold text-accent-orange">{name}</h4>
-        <p className="text-center text-sm text-foreground-secondary">
-          {description}
-        </p>
-      </hgroup>
     </div>
   );
 }
