2ec7d43b110ba53a833832af5c941b50dc7be486
Ivan <ivan.tihii1984@gmail.com>
2025-11-07 10:29:39 +0300

    fix(widgets): add useState

--- src/shared/ui/accordion.tsx
+++ src/shared/ui/accordion.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import { AccordionArrowIcon } from "../icons";
 
 type AccordionProps = {
@@ -8,6 +11,7 @@ type AccordionProps = {
 };
 
 export function Accordion({ items }: AccordionProps) {
+  const [isOpenIndex, setIsOpenIndex] = useState<number | null>(null);
   return (
     <div className="flex flex-col gap-5">
       {items.map(({ question, answer }) => {
@@ -15,13 +19,14 @@ export function Accordion({ items }: AccordionProps) {
           <div
             key={question}
             className="group mb-2 cursor-pointer items-center justify-between rounded-xl border border-accent-orange-light p-6 text-sm hover:border-accent-orange xl:text-lg"
+            onClick={(e) => e.currentTarget.classList.toggle("active")}
           >
             <div className="flex items-center justify-between font-semibold select-none">
               {question}
-              <AccordionArrowIcon className="transition-transform group-hover:rotate-180 [&>:first-child]:fill-accent-orange-light [&>:first-child]:group-hover:fill-accent-orange" />
+              <AccordionArrowIcon className="transition-transform group-[.active]:rotate-180 [&>:first-child]:fill-accent-orange-light [&>:first-child]:group-[.active]:fill-accent-orange" />
             </div>
 
-            <div className="grid grid-rows-[0fr] transition-[grid-template-rows] duration-400 group-hover:grid-rows-[1fr]">
+            <div className="grid grid-rows-[0fr] transition-[grid-template-rows] duration-400 group-[.active]:grid-rows-[1fr]">
               <div className="overflow-hidden">{answer}</div>
             </div>
           </div>
