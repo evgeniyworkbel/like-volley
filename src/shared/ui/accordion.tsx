9e8992b9d43ff6c2ee974bdf8430eef547a13a2d
Ivan <ivan.tihii1984@gmail.com>
2025-11-08 16:26:27 +0300

    refactor(shared): change Accordion

--- src/shared/ui/accordion.tsx
+++ src/shared/ui/accordion.tsx
@@ -8,45 +8,42 @@ type AccordionProps = {
   items: {
     question: string;
     answer: React.ReactNode;
+    id: number;
   }[];
 };
 
 export function Accordion({ items }: AccordionProps) {
-  const [isOpenIndex, setIsOpenIndex] = useState<number | null>(null);
+  const [openedItem, setOpenedItem] = useState<number>();
 
   return (
     <div className="flex flex-col gap-5">
-      {items.map(({ question, answer }, index) => {
-        const isOpen = isOpenIndex === index;
+      {items.map(({ question, answer, id }) => {
+        const isOpen = openedItem === id;
 
         return (
           <div
             key={question}
             className={cn(
-              "mb-2 cursor-pointer items-center justify-between rounded-xl border p-6 text-sm xl:text-lg",
+              "mb-2 cursor-pointer items-center justify-between rounded-xl border border-accent-orange-light p-6 text-sm xl:text-lg",
               {
                 "border-accent-orange": isOpen,
-                "border-accent-orange-light": !isOpen,
               },
             )}
-            onClick={() => setIsOpenIndex(isOpen ? null : index)}
+            onClick={() => setOpenedItem(isOpen ? undefined : id)}
           >
-            <div className="flex items-center justify-between font-semibold select-none">
+            <div className="flex items-center justify-between font-semibold select-none [&>:first-child]:fill-accent-orange-light">
               {question}
               <AccordionArrowIcon
                 className={cn("transition-transform", {
                   "rotate-180 [&>:first-child]:fill-accent-orange": isOpen,
-                  "[&>:first-child]:fill-accent-orange-light group-hover:[&>:first-child]:fill-accent-orange":
-                    !isOpen,
                 })}
               />
             </div>
             <div
               className={cn(
-                "grid transition-[grid-template-rows] duration-400",
+                "grid grid-rows-[0fr] transition-[grid-template-rows] duration-400",
                 {
                   "grid-rows-[1fr]": isOpen,
-                  "grid-rows-[0fr]": !isOpen,
                 },
               )}
             >
