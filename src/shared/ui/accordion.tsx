0e7da571f2311fe05f0c3aaa9f76f926a7bde5a8
Ivan <ivan.tihii1984@gmail.com>
2025-11-08 11:00:34 +0300

    fix(widgets): change behavior Accordion

--- src/shared/ui/accordion.tsx
+++ src/shared/ui/accordion.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { AccordionArrowIcon } from "../icons";
+import { cn } from "../lib";
 
 type AccordionProps = {
   items: {
@@ -12,21 +13,43 @@ type AccordionProps = {
 
 export function Accordion({ items }: AccordionProps) {
   const [isOpenIndex, setIsOpenIndex] = useState<number | null>(null);
+
   return (
     <div className="flex flex-col gap-5">
-      {items.map(({ question, answer }) => {
+      {items.map(({ question, answer }, index) => {
+        const isOpen = isOpenIndex === index;
+
         return (
           <div
             key={question}
-            className="group mb-2 cursor-pointer items-center justify-between rounded-xl border border-accent-orange-light p-6 text-sm hover:border-accent-orange xl:text-lg"
-            onClick={(e) => e.currentTarget.classList.toggle("active")}
+            className={cn(
+              "mb-2 cursor-pointer items-center justify-between rounded-xl border p-6 text-sm xl:text-lg",
+              {
+                "border-accent-orange": isOpen,
+                "border-accent-orange-light": !isOpen,
+              },
+            )}
+            onClick={() => setIsOpenIndex(isOpen ? null : index)}
           >
             <div className="flex items-center justify-between font-semibold select-none">
               {question}
-              <AccordionArrowIcon className="transition-transform group-[.active]:rotate-180 [&>:first-child]:fill-accent-orange-light [&>:first-child]:group-[.active]:fill-accent-orange" />
+              <AccordionArrowIcon
+                className={cn("transition-transform", {
+                  "rotate-180 [&>:first-child]:fill-accent-orange": isOpen,
+                  "[&>:first-child]:fill-accent-orange-light group-hover:[&>:first-child]:fill-accent-orange":
+                    !isOpen,
+                })}
+              />
             </div>
-
-            <div className="grid grid-rows-[0fr] transition-[grid-template-rows] duration-400 group-[.active]:grid-rows-[1fr]">
+            <div
+              className={cn(
+                "grid transition-[grid-template-rows] duration-400",
+                {
+                  "grid-rows-[1fr]": isOpen,
+                  "grid-rows-[0fr]": !isOpen,
+                },
+              )}
+            >
               <div className="overflow-hidden">{answer}</div>
             </div>
           </div>
