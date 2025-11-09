40d5b5aa71527a73f1e391700baf38a6c9e56ed5
Ivan <ivan.tihii1984@gmail.com>
2025-11-09 20:21:36 +0300

    refactor(widgets): change Banner

--- src/widgets/banner/ui/banner.tsx
+++ src/widgets/banner/ui/banner.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Banner() {
   return (
     <section className="flex justify-center px-5 py-[50px] xl:px-20">
-      <div className="relative flex aspect-[0.7] w-full justify-center rounded-2xl border-4 border-[oklch(0.6559_0.1604_257.81)] bg-accent-blue px-5 pt-4 xl:aspect-[4.3] xl:h-[297px] xl:items-center xl:justify-start xl:pt-0 xl:pl-10">
+      <div className="relative flex aspect-[0.7] w-full justify-center rounded-2xl border-4 border-[oklch(0.6559_0.1604_257.81)] bg-accent-blue px-5 pt-4 xl:aspect-[4.3] xl:h-[297px] xl:items-center xl:justify-start xl:px-10">
         <hgroup className="z-10 flex flex-col items-center gap-6 font-extrabold text-white xl:items-start">
           <Title className="text-4xl text-inherit">Приведи друга</Title>
           <p className="text-[15px] xl:text-2xl">
