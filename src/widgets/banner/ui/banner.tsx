98a67da37131e92fd8eaae54aa89b9df0ff0887e
Ivan <ivan.tihii1984@gmail.com>
2025-11-09 19:50:31 +0300

    refactor(widgets): change Banner

--- src/widgets/banner/ui/banner.tsx
+++ src/widgets/banner/ui/banner.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export function Banner() {
   return (
     <section className="flex justify-center px-5 py-[50px] xl:px-20">
-      <div className="relative aspect-[0.7] w-full rounded-2xl border-4 border-[oklch(0.6559_0.1604_257.81)] bg-accent-blue xl:aspect-[4.3] xl:h-[297px] xl:py-[52px] xl:pl-10">
-        <hgroup className="z-10 flex flex-col items-center gap-6 pt-4 font-extrabold text-white xl:items-start">
+      <div className="relative flex aspect-[0.7] w-full justify-center rounded-2xl border-4 border-[oklch(0.6559_0.1604_257.81)] bg-accent-blue pt-4 xl:aspect-[4.3] xl:h-[297px] xl:items-center xl:justify-start xl:pl-10">
+        <hgroup className="z-10 flex flex-col items-center gap-6 font-extrabold text-white xl:items-start">
           <Title className="text-4xl text-inherit">Приведи друга</Title>
           <p className="text-[15px] xl:text-2xl">
             Вы оба получите скидку в&nbsp;
