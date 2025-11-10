1ab8137e947ac026614ad781e36b142679f2fe9c
Ivan <ivan.tihii1984@gmail.com>
2025-11-10 13:37:15 +0300

    refactor(widgets): add hover for gallery-card

--- src/widgets/gallery/ui/gallery-сard.tsx
+++ src/widgets/gallery/ui/gallery-сard.tsx
@@ -8,10 +8,19 @@ type GalleryCardProps = GalleryItem;
 export function GalleryCard({ src, alt }: GalleryCardProps) {
   return (
     <Link
-      className="relative flex h-[366px] w-80 gap-5 overflow-hidden rounded-[18px] xl:w-[366px]"
+      className="group relative flex h-[366px] w-80 overflow-hidden rounded-[18px] xl:w-[366px]"
       href={contactsLinks.gallery.href}
     >
       <Image src={src} alt={alt} sizes="320px" fill />
+      <div className="absolute inset-0 flex items-center justify-center font-semibold">
+        <span className="text-transparent transition-colors duration-600 group-hover:text-white">
+          Смотреть все
+        </span>
+        <p className="absolute bottom-2 left-4 flex flex-col text-transparent transition-colors duration-600 group-hover:text-white">
+          <span>Тренировка перед матчем</span>
+          <span className="text-xs font-normal">23.08.20024</span>
+        </p>
+      </div>
     </Link>
   );
 }
