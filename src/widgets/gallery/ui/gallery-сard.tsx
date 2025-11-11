f0bdc05e2bde10754b7928f7242e591dd208862b
Ivan <ivan.tihii1984@gmail.com>
2025-11-11 10:46:10 +0300

    refactor(widgets): change GAlleryCards

--- src/widgets/gallery/ui/gallery-сard.tsx
+++ src/widgets/gallery/ui/gallery-сard.tsx
@@ -12,12 +12,10 @@ export function GalleryCard({ src, alt }: GalleryCardProps) {
       href={contactsLinks.gallery.href}
     >
       <Image src={src} alt={alt} sizes="320px" fill />
-      <div className="absolute inset-0 flex items-center justify-center font-semibold transition-colors duration-600 group-hover:bg-foreground/50">
-        <span className="text-transparent transition-colors duration-600 group-hover:text-white">
-          Смотреть все
-        </span>
+      <div className="absolute inset-0 flex items-center justify-center font-semibold text-white opacity-0 transition-colors duration-600 group-hover:bg-foreground/50 group-hover:opacity-100">
+        <span>Смотреть все</span>
         {/** @todo: поменять на актуальные данные потом*/}
-        {/* <p className="absolute bottom-2 left-4 flex flex-col text-transparent transition-colors duration-600 group-hover:text-white">
+        {/* <p className="absolute bottom-2 left-4 flex flex-col ">
           <span>Тренировка перед матчем</span>
           <span className="text-xs font-normal">23.08.20024</span>
         </p> */}
