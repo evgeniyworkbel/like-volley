c261104205681681b13879a3a24dc3f83a4b0889
Ivan Khristenko <ivan.tihii1984@gmail.com>
2025-11-16 12:08:52 +0300

    fix(widgets): add new data for coaches card

--- src/widgets/coaches/model/types.ts
+++ src/widgets/coaches/model/types.ts
@@ -3,4 +3,6 @@ import { ImageProps } from "next/image";
 export type Coach = Pick<ImageProps, "alt" | "src"> & {
   name: string;
   description: string;
+  city?: "Город: Брест" | "Город: Минск";
+  quote?: string;
 };
