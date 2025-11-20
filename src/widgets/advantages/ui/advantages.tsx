269019fefa9652909571c875b37da6a60a6cff13
Ivan <ivan.tihii1984@gmail.com>
2025-11-20 12:17:20 +0300

    fix(widgets): add Advantage table

--- src/widgets/advantages/ui/advantages.tsx
+++ src/widgets/advantages/ui/advantages.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export function Advantages() {
   return (
-    <section className="flex flex-col items-center justify-center gap-12 px-10 py-[74px] text-center text-sm md:flex-row md:flex-wrap xl:justify-between xl:gap-0 xl:px-20 xl:text-base">
+    <section className="flex max-w-[1146px] flex-col items-center justify-center gap-12 px-10 py-[74px] text-center text-sm md:flex-row md:flex-wrap xl:max-w-360 xl:justify-between xl:gap-0 xl:px-20 xl:text-base">
       {advantagesData.map(({ title, imageSrc, description, alt }) => {
         return (
           <div
             key={imageSrc}
-            className="flex max-w-80 flex-[1_0_calc(50%-24px)] flex-col items-center gap-3 xl:gap-4"
+            className="flex max-w-81 flex-col items-center gap-3 md:flex-[1_0_calc(50%-24px)] xl:gap-4"
           >
             <Image src={imageSrc} width={80} height={80} alt={alt} />
             <h3 className="font-bold">{title}</h3>
