190bd6a12d19182f4a35fc91f31513799a075a80
Ivan <ivan.tihii1984@gmail.com>
2025-11-21 10:08:24 +0300

    feat(widgets): add-advantages-md-width

--- src/widgets/advantages/model/types.ts
+++ src/widgets/advantages/model/types.ts
@@ -1,4 +1,5 @@
 export type AdvantageModel = {
+  id: number;
   title: string;
   imageSrc: string;
   description: string;
--- src/widgets/advantages/ui/advantages.tsx
+++ src/widgets/advantages/ui/advantages.tsx
@@ -1,18 +1,26 @@
+import { cn } from "@/shared/lib";
 import { advantagesData } from "../model/data";
 import Image from "next/image";
 
 export function Advantages() {
   return (
-    <section className="flex max-w-[1146px] flex-col items-center justify-center gap-12 px-10 py-[74px] text-center text-sm md:flex-row md:flex-wrap xl:max-w-360 xl:justify-between xl:gap-0 xl:px-20 xl:text-base">
-      {advantagesData.map(({ title, imageSrc, description, alt }) => {
+    <section className="grid grid-cols-1 justify-items-center gap-12 px-10 py-[74px] text-center text-sm md:grid-cols-2 xl:grid-cols-3 xl:gap-0 xl:px-20 xl:text-base">
+      {advantagesData.map(({ id, title, imageSrc, description, alt }) => {
         return (
           <div
             key={imageSrc}
-            className="flex max-w-81 flex-col items-center gap-3 md:flex-[1_0_calc(50%-24px)] xl:gap-4"
+            className={cn(
+              "flex h-44 w-81 flex-col items-center gap-3 xl:gap-4",
+              {
+                "md:col-span-2 xl:col-span-1": id === 3,
+              },
+            )}
           >
             <Image src={imageSrc} width={80} height={80} alt={alt} />
             <h3 className="font-bold">{title}</h3>
-            <h4 className="text-foreground-secondary">{description}</h4>
+            <h4 className="whitespace-pre-line text-foreground-secondary">
+              {description}
+            </h4>
           </div>
         );
       })}
