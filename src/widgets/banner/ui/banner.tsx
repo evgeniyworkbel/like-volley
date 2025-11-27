a3e4312cd6d85c32563897721d24b8bc20d4a380
Ivan <ivan.tihii1984@gmail.com>
2025-11-27 10:53:23 +0300

    fix(widgets): add tablet banner

--- src/widgets/banner/ui/banner.tsx
+++ src/widgets/banner/ui/banner.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Banner() {
   return (
     <section className="flex justify-center px-5 py-[50px] xl:px-20">
-      <div className="relative flex aspect-[0.66] w-full justify-center rounded-2xl border-4 border-[oklch(0.6559_0.1604_257.81)] bg-accent-blue px-5 pt-4 md:h-[297px] md:items-center md:justify-start xl:aspect-[3.77] xl:px-10">
+      <div className="relative flex aspect-[0.66] w-full justify-center rounded-2xl border-4 border-[oklch(0.6559_0.1604_257.81)] bg-accent-blue px-5 pt-4 md:aspect-[2.45] md:h-[297px] md:items-center md:justify-start xl:aspect-[4.3] xl:px-10">
         <hgroup className="z-10 flex flex-col items-center gap-6 font-extrabold text-white md:items-start md:gap-4 xl:gap-6">
           <Title className="text-4xl text-inherit md:text-[44px]">
             Приведи друга
@@ -15,14 +15,20 @@ export function Banner() {
           </p>
         </hgroup>
         <Image
-          className="object-cover object-bottom-right md:hidden"
+          className="object-bottom-right md:hidden"
           src="/banner/promo-mobile.webp"
           fill
           quality={100}
+          alt="Баннер синего цвета с декоратoивными иконками и сверху надпись 'приведи друга'"
+        />
+        <Image
+          className="hidden rounded-2xl object-cover object-right md:block xl:hidden"
+          src="/banner/promo-tablet.webp"
+          fill
           alt="Баннер синего цвета с декоративными иконками и сверху надпись 'приведи друга'"
         />
         <Image
-          className="hidden object-cover md:block md:object-right"
+          className="hidden rounded-2xl object-cover xl:block xl:object-right"
           src="/banner/promo-desktop.webp"
           fill
           alt="Баннер синего цвета с декоративными иконками и сверху надпись 'приведи друга'"
@@ -31,3 +37,4 @@ export function Banner() {
     </section>
   );
 }
+//  Что делать с лфтом tablet image ?
