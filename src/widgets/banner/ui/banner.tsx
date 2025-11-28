58f37fdcf6f5b659efd4390b1100982c66c4f0c2
Ivan <ivan.tihii1984@gmail.com>
2025-11-28 16:03:51 +0300

    refactor(widgets): refined h for div

--- src/widgets/banner/ui/banner.tsx
+++ src/widgets/banner/ui/banner.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Banner() {
   return (
     <section className="flex justify-center px-5 py-[50px] xl:px-20">
-      <div className="relative flex aspect-[0.66] w-full justify-center rounded-2xl border-4 border-[oklch(0.6559_0.1604_257.81)] bg-accent-blue px-5 pt-4 md:aspect-[2.45] md:h-[297px] md:items-center md:justify-start xl:aspect-[4.3] xl:px-10">
+      <div className="relative flex aspect-[0.66] w-full justify-center rounded-2xl border-4 border-[oklch(0.6559_0.1604_257.81)] bg-accent-blue px-5 pt-4 md:aspect-[2.45] md:items-center md:justify-start xl:aspect-[4.0] xl:px-10">
         <hgroup className="z-10 flex flex-col items-center gap-6 font-extrabold text-white md:items-start md:gap-4 xl:gap-6">
           <Title className="text-4xl text-inherit md:text-[44px]">
             Приведи друга
