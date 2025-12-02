d8edf8360fc1a1161e15556294d4d47f2d33d518
Ivan <ivan.tihii1984@gmail.com>
2025-12-02 11:02:28 +0300

    refactor(widgets): refind style page for error

--- src/widgets/hero/ui/hero.tsx
+++ src/widgets/hero/ui/hero.tsx
@@ -86,7 +86,7 @@ export function Hero() {
               "bg-accent-orange": ageGroup === "adults",
             },
           )}
-          onClick={handlezscClick}
+          onClick={handleClick}
         >
           Записаться
         </Button>
