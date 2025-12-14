ff637a7a317ba7c070d8975bda621f587a1d558d
Ivan Khristenko <ivan.tihii1984@gmail.com>
2025-12-14 13:13:52 +0300

    refactor(widgets): change styles for Contacts

--- src/widgets/sign-up-for-workout/ui/contact-form2.tsx
+++ src/widgets/sign-up-for-workout/ui/contact-form2.tsx
@@ -9,7 +9,7 @@ export function Form2() {
   };
 
   return (
-    <div className="relative h-175 max-w-175 overflow-hidden rounded-3xl bg-accent-blue p-6 md:mx-auto xl:mx-0 xl:h-[774px]">
+    <div className="relative h-175 max-w-175 overflow-hidden rounded-3xl bg-accent-blue p-6 md:mx-auto md:h-[774px] xl:mx-0">
       <Image
         className="absolute bottom-0 left-1/2 w-[460px] -translate-x-1/2 -translate-y-20 scale-[1.6] xl:w-full xl:translate-y-0 xl:scale-100"
         width={700}
@@ -18,7 +18,7 @@ export function Form2() {
         alt="Фото игроков в волейбол (взрослые и дети)"
       />
       <div className="relative flex h-full flex-col items-center justify-between">
-        <Title className="text-center text-[28px] text-accent-green xl:text-[44px]">
+        <Title className="text-center text-[28px] text-accent-green md:text-[44px]">
           Приходи на первую тренировку бесплатно!
         </Title>
         <Button
--- src/widgets/sign-up-for-workout/ui/sign-up-for-workout.tsx
+++ src/widgets/sign-up-for-workout/ui/sign-up-for-workout.tsx
@@ -1,5 +1,4 @@
 import { Title } from "@/shared/ui";
-import { contactFormId } from "@/shared/constants";
 import { Contact } from "./contact";
 // import { ContactForm } from "./contact-form";
 import { contactsData } from "../model/data";
@@ -7,12 +6,9 @@ import { Form2 } from "./contact-form2";
 
 export function SignUpForWorkout() {
   return (
-    <section
-      id={contactFormId}
-      className="flex flex-col gap-12 rounded-[20px] bg-accent-orange-muted px-5 py-10 md:mx-auto md:flex-row md:flex-wrap xl:mx-0 xl:flex-nowrap xl:justify-between xl:px-20 xl:py-[90px]"
-    >
-      <div className="mx-auto flex flex-col gap-10 md:max-w-182 md:flex-row md:flex-wrap xl:max-w-[494px] xl:py-38">
-        <div className="flex flex-col gap-4 text-center md:mx-auto xl:mx-0">
+    <section className="flex flex-col gap-12 rounded-[20px] bg-accent-orange-muted px-5 py-10 md:items-center xl:flex-row xl:justify-between xl:px-20 xl:py-[90px]">
+      <div className="flex flex-col gap-10 md:items-center xl:max-w-[494px] xl:items-start xl:gap-12">
+        <div className="flex max-w-182 flex-col gap-4 text-center xl:pt-28">
           <Title className="xl:text-left">
             <span className="text-accent-orange">Запишись&nbsp;</span>
             на первую тренировку
@@ -21,16 +17,19 @@ export function SignUpForWorkout() {
             Свяжитесь с нами для получения персональной консультации
           </p>
         </div>
-        {contactsData.map(({ title, imageSrc, alt, href, label }) => (
-          <Contact
-            key={title}
-            title={title}
-            imageSrc={imageSrc}
-            href={href}
-            alt={alt}
-            label={label}
-          />
-        ))}
+
+        <div className="flex flex-col gap-6 md:max-w-140 md:flex-row md:flex-wrap md:items-center md:justify-center xl:flex-col xl:items-start">
+          {contactsData.map(({ title, imageSrc, alt, href, label }) => (
+            <Contact
+              key={title}
+              title={title}
+              imageSrc={imageSrc}
+              href={href}
+              alt={alt}
+              label={label}
+            />
+          ))}
+        </div>
       </div>
       {/** @todo: удалить Form2 когда будет готова интеграция нашей формы с amoCRM */}
       <Form2 />
