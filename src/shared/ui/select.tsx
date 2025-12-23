237653fdda82e2af5f99ee683db516fe23637f62
Ivan <ivan.tihii1984@gmail.com>
2025-12-23 09:05:26 +0300

    refactor(widgets): add comments for new tasks Select.tsx

--- src/shared/ui/select.tsx
+++ src/shared/ui/select.tsx
@@ -1,6 +1,9 @@
-import * as React from "react";
+import React from "react";
 import { Select } from "@base-ui/react/select";
 
+{
+  /** @todo:  удалить и прокидывать items через пропс */
+}
 const fonts = [
   { label: "Select font", value: null },
   { label: "Sans-serif", value: "sans" },
@@ -10,6 +13,7 @@ const fonts = [
 ];
 
 export default function ExampleSelect() {
+  /** @todo:   убрать default и переназвать компонент с ExampleSelect на Select (селект из библиотеки можешь в импорте переименовать с Select на BaseSelect) */
   return (
     <Select.Root items={fonts}>
       <Select.Trigger className="border-gray-200 text-gray-900 hover:bg-gray-100 focus-visible:outline-blue-800 data-popup-open:bg-gray-100 flex h-10 min-w-36 items-center justify-between gap-3 rounded-md border bg-[canvas] pr-3 pl-3.5 text-base select-none focus-visible:outline-2 focus-visible:-outline-offset-1">
@@ -45,6 +49,7 @@ export default function ExampleSelect() {
 }
 
 function ChevronUpDownIcon(props: React.ComponentProps<"svg">) {
+  /** @todo:   убрать отсюда иконки и положить в icons папку  */
   return (
     <svg
       width="8"
@@ -62,6 +67,7 @@ function ChevronUpDownIcon(props: React.ComponentProps<"svg">) {
 }
 
 function CheckIcon(props: React.ComponentProps<"svg">) {
+  /** @todo:   убрать отсюда иконки и положить в icons папку  */
   return (
     <svg fill="currentcolor" width="10" height="10" viewBox="0 0 10 10" {...props}>
       <path d="M9.1603 1.12218C9.50684 1.34873 9.60427 1.81354 9.37792 2.16038L5.13603 8.66012C5.01614 8.8438 4.82192 8.96576 4.60451 8.99384C4.3871 9.02194 4.1683 8.95335 4.00574 8.80615L1.24664 6.30769C0.939709 6.02975 0.916013 5.55541 1.19372 5.24822C1.47142 4.94102 1.94536 4.91731 2.2523 5.19524L4.36085 7.10461L8.12299 1.33999C8.34934 0.993152 8.81376 0.895638 9.1603 1.12218Z" />
