0d65b337e3d24afe2ada2af555ec260588cbffb0
Ivan <ivan.tihii1984@gmail.com>
2025-12-25 13:44:17 +0300

    feat(widgets): height Header 92px

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -6,7 +6,7 @@ import { Navbar } from "./navbar";
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-40 flex items-center justify-between rounded-b-[14px] bg-white py-6 pr-12 pl-6 text-accent-orange shadow-[1px_2px_6px_0_oklch(0_0_0/0.12)] xl:px-20">
+    <header className="sticky top-0 z-40 flex items-center justify-between rounded-b-[14px] bg-white py-2 pr-12 pl-6 text-accent-orange shadow-[1px_2px_6px_0_oklch(0_0_0/0.12)] xl:px-20">
       <Link href={navLinks.home.href}>
         <Image src="/logo.svg" width={122} height={76} alt="Логотип школы волейбола Like Volley" />
       </Link>
