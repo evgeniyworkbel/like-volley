a9f66dbbcbf93460b14319193ad2a5190a58f0ff
Ivan <ivan.tihii1984@gmail.com>
2025-12-25 10:43:04 +0300

    feat(widgets): add sticky for Header,remove className for Image

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -6,15 +6,9 @@ import { Navbar } from "./navbar";
 
 export function Header() {
   return (
-    <header className="flex items-center justify-between rounded-b-[14px] py-6 pr-12 pl-6 text-accent-orange shadow-[1px_2px_6px_0_oklch(0_0_0/0.12)] xl:px-20">
+    <header className="sticky top-0 z-40 flex items-center justify-between rounded-b-[14px] bg-white py-6 pr-12 pl-6 text-accent-orange shadow-[1px_2px_6px_0_oklch(0_0_0/0.12)] xl:px-20">
       <Link href={navLinks.home.href}>
-        <Image
-          className="z-20"
-          src="/logo.svg"
-          width={122}
-          height={76}
-          alt="Логотип школы волейбола Like Volley"
-        />
+        <Image src="/logo.svg" width={122} height={76} alt="Логотип школы волейбола Like Volley" />
       </Link>
       <Navbar className="hidden xl:flex" />
       <MobileMenu />
