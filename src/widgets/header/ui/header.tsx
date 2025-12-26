4d51f9ff26bc56f6613a99f41224f5d3354e795e
Ivan <ivan.tihii1984@gmail.com>
2025-12-26 11:39:19 +0300

    refactor(widgets): remove isOpen in header for realize Blur effect for Header

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,17 +1,28 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { navLinks } from "@/shared/constants";
 import { MobileMenu } from "./mobile-menu";
 import { Navbar } from "./navbar";
+import { cn } from "@/shared/lib";
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <header className="sticky top-0 z-40 flex items-center justify-between rounded-b-[14px] bg-white py-2 pr-12 pl-6 text-accent-orange shadow-[1px_2px_6px_0_oklch(0_0_0/0.12)] xl:px-20">
+    <header
+      className={cn(
+        "sticky top-0 isolate z-40 flex items-center justify-between rounded-b-[14px] bg-background/60 py-2 pr-12 pl-6 text-accent-orange shadow-[1px_2px_6px_0_oklch(0_0_0/0.12)] backdrop-blur-sm xl:px-20",
+        { "bg-background backdrop-blur-none": isOpen },
+      )}
+    >
       <Link href={navLinks.home.href}>
         <Image src="/logo.svg" width={122} height={76} alt="Логотип школы волейбола Like Volley" />
       </Link>
       <Navbar className="hidden xl:flex" />
-      <MobileMenu />
+      <MobileMenu isOpen={isOpen} setIsOpenAction={setIsOpen} />
     </header>
   );
 }
--- src/widgets/header/ui/mobile-menu.tsx
+++ src/widgets/header/ui/mobile-menu.tsx
@@ -1,17 +1,20 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { Button } from "@/shared/ui";
 import { cn } from "@/shared/lib";
 import { CloseMenuIcon } from "./close-menu-icon";
 import { Navbar } from "./navbar";
 import { BurgerMenuIcon } from "./burger-menu-icon";
 
-export function MobileMenu() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+type MobileMenuModel = {
+  isOpen: boolean;
+  setIsOpenAction: (open: boolean) => void;
+};
 
-  const openMenu = () => setIsOpen(true);
-  const closeMenu = () => setIsOpen(false);
+export function MobileMenu({ isOpen, setIsOpenAction }: MobileMenuModel) {
+  const openMenu = () => setIsOpenAction(true);
+  const closeMenu = () => setIsOpenAction(false);
 
   useEffect(() => {
     if (isOpen) {
@@ -22,7 +25,7 @@ export function MobileMenu() {
   }, [isOpen]);
 
   return (
-    <div className="z-20 flex xl:hidden">
+    <div className="flex xl:hidden">
       {!isOpen && (
         <Button onClick={openMenu}>
           <BurgerMenuIcon />
@@ -35,7 +38,7 @@ export function MobileMenu() {
       )}
       <Navbar
         className={cn(
-          "fixed -top-7 left-0 z-10 w-full flex-col rounded-b-2xl bg-background/60 pb-5 font-shantell text-2xl backdrop-blur-sm transition-transform duration-500 ease-in-out [&>*:last-child]:rounded-[40px] [&>*:last-child]:bg-accent-blue [&>*:last-child]:px-12 [&>*:last-child]:py-4 [&>*:last-child]:text-white [&>*:last-child]:no-underline",
+          "fixed -top-7 left-0 isolate w-full flex-col rounded-b-2xl bg-background/60 pb-5 font-shantell text-2xl backdrop-blur-sm transition-transform duration-500 ease-in-out [&>*:last-child]:rounded-[40px] [&>*:last-child]:bg-accent-blue [&>*:last-child]:px-12 [&>*:last-child]:py-4 [&>*:last-child]:text-white [&>*:last-child]:no-underline",
           {
             "translate-y-30": isOpen,
             "-translate-y-full": !isOpen,
