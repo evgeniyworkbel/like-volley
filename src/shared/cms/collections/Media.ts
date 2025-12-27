2e2ea0ef21e3367247f586d547c278cb2e459e0f
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-12-27 21:25:43 +0300

    feat(shared): localize media collection, add description for field

--- src/shared/cms/collections/Media.ts
+++ src/shared/cms/collections/Media.ts
@@ -2,6 +2,9 @@ import type { CollectionConfig } from "payload";
 
 export const Media: CollectionConfig = {
   slug: "media",
+  labels: {
+    plural: { en: "Media", ru: "Медиа" },
+  },
   access: {
     read: () => true,
   },
@@ -9,6 +12,11 @@ export const Media: CollectionConfig = {
     {
       name: "alt",
       type: "text",
+      label: { en: "Alt text (for seo)", ru: "Альтернативный текст (для seo)" },
+      admin: {
+        description:
+          "Как правильно написать alt текст для картинки https://doka.guide/html/alt/#osnovnye-pravila-zapolneniya",
+      },
       required: true,
     },
   ],
