602e382a434f71744705c3634533b7ef06c349eb
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2025-12-28 23:37:37 +0300

    fix(shared): teach formatPhone() fn to work with raw phone number

--- src/shared/lib/format-phone.ts
+++ src/shared/lib/format-phone.ts
@@ -1,11 +1,11 @@
 export const formatPhone = (phoneNumber: string): string => {
-  if (!phoneNumber.startsWith("+375") || phoneNumber.length !== 13) {
-    throw new Error("Invalid phone number format. Expected +375 followed by 9 digits.");
+  if (!phoneNumber.startsWith("375") || phoneNumber.length !== 12) {
+    throw new Error("Invalid phone number format. Expected 375 followed by 9 digits.");
   }
 
-  const countryCode = phoneNumber.substring(0, 4);
-  const operatorCode = phoneNumber.substring(4, 6);
-  const restOfNumber = phoneNumber.substring(6);
+  const countryCode = phoneNumber.substring(0, 3);
+  const operatorCode = phoneNumber.substring(3, 5);
+  const restOfNumber = phoneNumber.substring(5);
   const restOfNumberWithHyphens = [
     restOfNumber.substring(0, 3),
     restOfNumber.substring(3, 5),
