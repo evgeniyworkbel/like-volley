1c8e6f2f7873afe0d8684e1dcae47981e2f4b233
Ivan <ivan.tihii1984@gmail.com>
2025-12-29 12:28:54 +0300

    feat(widgets): add new images for contacts into footer

--- src/widgets/footer/ui/contacts.tsx
+++ src/widgets/footer/ui/contacts.tsx
@@ -3,36 +3,36 @@ import Image from "next/image";
 
 export function Contacts() {
   return (
-    <div className="flex gap-6">
+    <div className="flex gap-4">
       <a href={contactsLinks.phone.href} target="_blank" rel="noopener noreferrer">
         <Image
           src="/phone.svg"
-          width={68}
-          height={68}
+          width={46}
+          height={46}
           alt="Декоративная иконка телефона школы волейбола Like Volley"
         />
       </a>
       <a href={contactsLinks.locations.href} target="_blank" rel="noopener noreferrer">
         <Image
           src="/location.svg"
-          width={68}
-          height={68}
+          width={46}
+          height={46}
           alt="Декоративная иконка локации школы волейбола Like Volley"
         />
       </a>
       <a href={contactsLinks.instagram.href} target="_blank" rel="noopener noreferrer">
         <Image
           src="/instagram.svg"
-          width={68}
-          height={68}
+          width={46}
+          height={46}
           alt="Декоративная иконка Instagram школы волейбола Like Volley"
         />
       </a>
       <a href={contactsLinks.telegram.href} target="_blank" rel="noopener noreferrer">
         <Image
           src="/telegram.svg"
-          width={68}
-          height={68}
+          width={46}
+          height={46}
           alt="Декоративная иконка telegram школы волейбола Like Volley"
         />
       </a>
