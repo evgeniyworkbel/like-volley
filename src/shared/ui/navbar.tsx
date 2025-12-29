f0c40ccf201ec229c1f171c57e8e846e4e56070f
Ivan Khristenko <ivan.tihii1984@gmail.com>
2025-12-29 20:39:46 +0300

    fix(shared): add a for link  phone number

--- src/shared/ui/navbar.tsx
+++ src/shared/ui/navbar.tsx
@@ -36,9 +36,9 @@ export function Navbar({ className, onLinkClick }: NavbarProps) {
       <Link href={navLinks.gallerySection.href} onClick={onLinkClick}>
         {navLinks.gallerySection.title}
       </Link>
-      <Link href={navLinks.phone.href} onClick={onLinkClick}>
+      <a href={navLinks.phone.href} onClick={onLinkClick}>
         {navLinks.phone.title}
-      </Link>
+      </a>
     </nav>
   );
 }
