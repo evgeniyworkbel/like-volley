901cb6847497174ba3259a4e2c6547babfa3f927
Ivan <ivan.tihii1984@gmail.com>
2025-12-30 10:53:20 +0300

    feat(widgets): add the component city-botton

--- src/features/locations-map/ui/locations-map.tsx
+++ src/features/locations-map/ui/locations-map.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { Chips } from "@/widgets/locations/ui/сhips ";
+
 import { MapProvider } from "@/shared/providers";
 import type { YMapLocationRequest } from "@/shared/lib";
 
@@ -14,6 +14,7 @@ import {
 } from "../model/data";
 import { useState } from "react";
 import { CompanyCities } from "../model/types";
+import { CityButton } from "./city-button";
 
 const Map = dynamic(() => import("@/shared/ui/map").then((module) => module.Map));
 
@@ -22,16 +23,25 @@ export function LocationsMap() {
   const location: YMapLocationRequest = { bounds: locationsBounds[city] };
 
   return (
-    <div className="flex h-[522px] w-full flex-col items-center gap-5 overflow-hidden rounded-[20px] xl:h-[712px]">
-      <Chips city={city} setCity={setCity} />
-      <MapProvider>
-        <Map
-          location={location}
-          // restrictMapArea={restrictArea[city]}
-          // zoomRange={locationsZoomRange}
-          markers={locationsMarkers[city]}
-        />
-      </MapProvider>
+    <div className="flex w-full flex-col items-center gap-5">
+      <div className="flex gap-6">
+        <CityButton newCity="minsk" selectedCity={city} onClick={() => setCity("minsk")}>
+          Минск
+        </CityButton>
+        <CityButton newCity="brest" selectedCity={city} onClick={() => setCity("brest")}>
+          Брест
+        </CityButton>
+      </div>
+      <div className="flex h-[522px] w-full flex-col gap-5 overflow-hidden rounded-[20px] xl:h-178">
+        <MapProvider>
+          <Map
+            location={location}
+            // restrictMapArea={restrictArea[city]}
+            // zoomRange={locationsZoomRange}
+            markers={locationsMarkers[city]}
+          />
+        </MapProvider>
+      </div>
     </div>
   );
 }
