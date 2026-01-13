e8361a94660ef2938041d68dcce3d1ce9b0084a4
Ivan <ivan.tihii1984@gmail.com>
2026-01-13 09:02:36 +0300

    refactor(widgets): change structure of styles css for Footer-nav

--- src/widgets/footer/ui/footer-nav.tsx
+++ src/widgets/footer/ui/footer-nav.tsx
@@ -3,10 +3,10 @@ import { navLinks } from "@/shared/constants";
 
 export function FooterNav() {
   return (
-    <div className="flex flex-col items-center justify-center gap-[50px] text-sm md:flex-row md:items-start md:gap-33 xl:justify-between xl:gap-[50px]">
+    <div className="flex flex-col items-center justify-center gap-[50px] md:flex-row md:items-start md:gap-33 xl:justify-between xl:gap-[50px] [&_li:hover]:underline [&_li:hover]:underline-offset-2">
       <div className="flex flex-col gap-[18px] md:gap-7 xl:items-start">
-        <h2>Навигация сайта</h2>
-        <ul className="flex flex-col items-center gap-[18px] text-lg font-bold uppercase md:items-start xl:text-lg [&>a:hover]:underline">
+        <h2 className="text-sm">Навигация сайта</h2>
+        <ul className="flex flex-col items-center gap-[18px] text-lg font-bold uppercase md:items-start xl:text-lg">
           <li>
             <Link href={navLinks.locationsSection.href}>{navLinks.locationsSection.title}</Link>
           </li>
@@ -23,7 +23,7 @@ export function FooterNav() {
       </div>
       <div className="flex flex-col items-center gap-[18px] text-[18px] md:items-start md:gap-7">
         <h2 className="text-sm">Документы</h2>
-        <ul className="flex flex-col items-center gap-[18px] font-bold uppercase md:items-start [&>li:hover]:underline [&>li:hover]:underline-offset-2">
+        <ul className="flex flex-col items-center gap-[18px] font-bold uppercase md:items-start">
           <li>
             <Link href={navLinks.policy.href}>{navLinks.policy.title}</Link>
           </li>
