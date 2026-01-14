7479a6712a629873b977fd4c72689d3f0cca6a77
Ivan <ivan.tihii1984@gmail.com>
2026-01-14 09:27:37 +0300

    refactor(widgets): change structure of Hero, remove meta.tsx

--- src/widgets/hero/ui/hero.tsx
+++ src/widgets/hero/ui/hero.tsx
@@ -7,49 +7,62 @@ import { cn } from "@/shared/lib";
 import { Title } from "@/shared/ui";
 import { signUpForWorkoutSectionId } from "@/shared/constants";
 import { HeroTab } from "./hero-tab";
-import { heroData } from "../model/meta";
 
 export function Hero() {
-  const [ageGroup, setAgeGroup] = useState<"children" | "adults">("children");
+  const [ageGroup, setAgeGroup] = useState<"childs" | "adults">("adults");
 
   return (
     <section className="flex flex-col px-5 pt-5 pb-12.5 text-center text-white md:pt-12.5 xl:px-20">
       <div className="flex gap-4 px-4 xl:justify-start xl:gap-0 xl:px-0">
         <HeroTab
           className={cn({
-            "bg-accent-orange": ageGroup === "children",
-            "bg-none text-accent-orange": ageGroup === "adults",
-          })}
-          onClick={() => setAgeGroup("children")}
-        >
-          Для детей
-        </HeroTab>
-        <HeroTab
-          className={cn({
-            "bg-none text-accent-blue": ageGroup === "children",
+            "bg-none text-accent-blue": ageGroup === "childs",
             "bg-accent-blue": ageGroup === "adults",
           })}
           onClick={() => setAgeGroup("adults")}
         >
           Для взрослых
         </HeroTab>
+        <HeroTab
+          className={cn({
+            "bg-accent-orange": ageGroup === "childs",
+            "bg-none text-accent-orange": ageGroup === "adults",
+          })}
+          onClick={() => setAgeGroup("childs")}
+        >
+          Для детей
+        </HeroTab>
       </div>
       <div
         className={cn(
-          "flex flex-col items-center gap-4 overflow-hidden rounded-[20px] bg-accent-orange p-5 md:relative md:h-199 md:gap-6 md:p-9 xl:h-116.5 xl:items-start xl:justify-center",
+          "flex flex-col items-center gap-4 overflow-hidden rounded-[20px] p-5 md:relative md:h-199 md:gap-6 md:p-9 xl:h-116.5 xl:items-start xl:justify-center",
           {
+            "bg-accent-orange": ageGroup === "childs",
             "bg-accent-blue": ageGroup === "adults",
           },
         )}
       >
         <div className="flex flex-col">
-          <Title className="z-10 leading-[0.85] text-inherit md:max-w-170 md:text-center md:text-[58px] xl:text-left xl:leading-none">
-            {heroData[ageGroup].title}
-          </Title>
+          {ageGroup === "childs" && (
+            <Title className="z-10 leading-[0.85] text-inherit md:max-w-170 md:text-center md:text-[58px] xl:text-left xl:leading-none">
+              Будущие чемпионы растут&nbsp;
+              <span className="text-accent-green">здесь</span>
+            </Title>
+          )}
+          {ageGroup === "adults" && (
+            <Title className="z-10 leading-[0.85] text-inherit md:text-center md:text-[58px] xl:text-left xl:leading-none">
+              Живи, играй,
+              <br />
+              <span className="text-accent-green">Побеждай!</span>
+            </Title>
+          )}
         </div>
-        {ageGroup === "children" && (
+        {ageGroup === "childs" && (
           <div className="flex flex-col items-center">
-            {heroData.children.subtitle}
+            <p className="z-10 flex max-w-2xs flex-col text-sm md:mb-6 md:max-w-170 md:text-lg xl:m-0 xl:text-start">
+              Профессиональная подготовка юных спортсменов. Развиваем координацию, характер и
+              командный дух в игровой форме.
+            </p>
             <Image
               className="md:absolute md:bottom-0 xl:right-5"
               src="/hero/children.webp"
@@ -62,7 +75,10 @@ export function Hero() {
         )}
         {ageGroup === "adults" && (
           <div className="flex flex-col items-center">
-            {heroData.adults.subtitle}
+            <p className="z-10 flex max-w-2xs flex-col text-sm md:mb-6 md:max-w-149 md:text-lg xl:m-0 xl:text-start">
+              Тренировки для любого уровня: от новичков до профи.
+              <br /> Улучшаем технику, тактику и физическую форму в удобное время.
+            </p>
             <Image
               className="md:absolute md:bottom-0 xl:right-5"
               src="/hero/adults.webp"
@@ -76,8 +92,9 @@ export function Hero() {
         <Link
           href={`#${signUpForWorkoutSectionId}`}
           className={cn(
-            "rounded-3xl bg-accent-blue px-19 py-2.5 text-[16px] font-semibold md:rounded-[40px] md:px-28 md:py-4 md:text-xl md:font-medium",
+            "rounded-3xl px-19 py-2.5 text-[16px] font-medium md:rounded-[40px] md:px-28 md:py-4 md:text-xl",
             {
+              "bg-accent-blue": ageGroup === "childs",
               "bg-accent-orange": ageGroup === "adults",
             },
           )}
