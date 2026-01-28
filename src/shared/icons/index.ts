22ead0508c411b3bb34946e0cb6901c0f9bccd32
Ivan <ivan.tihii1984@gmail.com>
2026-01-28 09:12:08 +0300

    refactor(widgets, shared): remove descriptions-foto components

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -2,4 +2,3 @@ export { AccordionArrowIcon } from "./accordion-arrow-icon";
 export { CarouselArrowIcon } from "./carousel-arrow-icon";
 export { ScrollToTopArrowIcon } from "./scroll-to-top-arrow-icon";
 export { RightIcon } from "./right-icon";
-export { DiagonallyIcon } from "./diagonally-icon";
--- src/widgets/gallery/ui/gallery-card.tsx
+++ src/widgets/gallery/ui/gallery-card.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { GalleryItemModel } from "../model/types";
-// import { DescriptionsFoto } from "./descriptions-foto";
 
 type GalleryCardProps = GalleryItemModel;
 
@@ -8,7 +7,6 @@ export function GalleryCard({ src, alt }: GalleryCardProps) {
   return (
     <div className="relative flex aspect-[0.874] h-91.5 max-w-91 items-end overflow-hidden rounded-[18px] md:aspect-square">
       <Image src={src} alt={alt} sizes="364px" fill />
-      {/* <DescriptionsFoto /> */}
     </div>
   );
 }
