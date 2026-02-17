6f366ab164ed83730bf9dcafe5d5e459168940f6
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2026-02-17 17:39:54 +0300

    fix(app): repair mapping data to avoid db error

--- src/app/(frontend)/(main)/policy/page.tsx
+++ src/app/(frontend)/(main)/policy/page.tsx
@@ -1,19 +1,23 @@
-import { RichText } from "@payloadcms/richtext-lexical/react";
 import { getPayloadClient } from "@/shared/cms";
+import { convertLexicalToHTML } from "@payloadcms/richtext-lexical/html";
 
 export default async function Policy() {
   const payload = await getPayloadClient();
-  const { title, description } = await payload.findGlobal({
+  const policy = await payload.findGlobal({
     slug: "policy",
   });
-
-  console.log(title, description);
+  const { title, description } = policy;
 
   return (
     <article className="container mx-auto max-w-4xl">
       <h1 className="pt-5 text-center text-2xl font-extrabold">{title}</h1>
       <section className="flex flex-col items-center p-5 text-sm leading-6">
-        {description && <RichText data={description} className="flex flex-col indent-6" />}
+        {description && (
+          <p
+            dangerouslySetInnerHTML={{ __html: convertLexicalToHTML({ data: description }) }}
+            className="flex flex-col indent-6"
+          />
+        )}
       </section>
     </article>
   );
