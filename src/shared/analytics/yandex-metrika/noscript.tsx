9a5b83146ca17aa4f4e301fc1c5848804f4d70fd
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2026-03-01 14:43:30 +0300

    build(shared): log public env to debug

--- src/shared/analytics/yandex-metrika/noscript.tsx
+++ src/shared/analytics/yandex-metrika/noscript.tsx
@@ -1,6 +1,12 @@
 import { YM_COUNTER_ID } from "./config";
 
 export function YandexMetrikaNoscript() {
+  console.log("noscript YM_COUNTER_ID", YM_COUNTER_ID);
+  console.log(
+    "noscript process.env.NEXT_PUBLIC_YANDEX_METRIKA_COUNTER_ID",
+    process.env.NEXT_PUBLIC_YANDEX_METRIKA_COUNTER_ID,
+  );
+
   return (
     <noscript
       dangerouslySetInnerHTML={{
