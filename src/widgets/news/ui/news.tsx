a2f31d16eacb2716a4c0044738bba5d3c1e67098
Ivan <ivan.tihii1984@gmail.com>
2026-03-18 14:24:59 +0300

    fix(widgets): change the title change the title position

--- src/widgets/news/ui/news.tsx
+++ src/widgets/news/ui/news.tsx
@@ -7,14 +7,16 @@ export function News() {
   // @todo реализовать получение через cms
   return (
     <section className="flex flex-col items-center justify-center gap-3 px-5 py-12.5 xl:px-20">
-      <Title className="text-4xl text-accent-orange md:text-[44px]">Новости</Title>
+      <hgroup className="flex flex-col items-center gap-3 text-center">
+        <Title className="text-4xl text-accent-orange md:text-[44px]">Новости</Title>
+        <p className="text-base font-bold xl:text-2xl">
+          «Лайк Воллей» выходит на международную арену: едем на «Кубок Дружбы» в Новосибирск!
+        </p>
+      </hgroup>
 
-      <div className="relative flex h-121 w-full justify-center overflow-hidden px-5 md:justify-start md:py-0 xl:h-213.25 xl:px-10">
-        <div className="z-10 flex max-w-73.75 flex-col gap-2 pt-6 font-extrabold text-white md:gap-4 xl:max-w-135 xl:gap-6 xl:pt-7">
-          <p className="text-base xl:text-2xl">
-            «Лайк Воллей» выходит на международную арену: едем на «Кубок Дружбы» в Новосибирск!
-          </p>
-          <Link href={navLinks.news.href} className="text-base text-accent-orange xl:text-2xl">
+      <div className="relative flex h-121 w-full items-end justify-center overflow-hidden px-5 md:justify-start xl:h-213.25 xl:px-10">
+        <div className="z-10 flex max-w-73.75 flex-col gap-2 pt-6 pb-5 font-extrabold text-white md:gap-4 xl:max-w-129 xl:gap-6">
+          <Link href={navLinks.news.href} className="text-xl backdrop-blur-sm xl:text-3xl">
             Подробнее...
           </Link>
         </div>
