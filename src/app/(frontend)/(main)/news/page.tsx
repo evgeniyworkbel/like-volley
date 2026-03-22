b5d3e30ba2ab90542ead74b570b076218d7f438b
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2026-03-22 23:55:05 +0300

    refactor(app): relayout news page

--- src/app/(frontend)/(main)/news/page.tsx
+++ src/app/(frontend)/(main)/news/page.tsx
@@ -4,35 +4,30 @@ import Link from "next/link";
 export default function News() {
   // @todo реализовать получение через cms
   // @todo при cms id далить
-  const newsId = "2";
+  const newsId = "1";
 
   return (
-    <article className="flex flex-col">
-      <section className="flex flex-col items-center gap-4 px-5 py-12.5 xl:gap-6 xl:px-20">
-        <h1 className="font-shantell text-4xl font-bold text-accent-orange md:text-6xl">Новости</h1>
-        <div className="relative flex h-121 w-full items-end justify-center overflow-hidden rounded-[20px] xl:h-213.25">
+    <section className="flex flex-col items-center gap-5 px-5 py-10 md:gap-10 xl:gap-31 xl:px-20">
+      <h1 className="font-shantell text-4xl font-bold text-accent-orange md:text-6xl">Новости</h1>
+      <section className="grid gap-5 xl:grid-cols-3">
+        <Link className="flex flex-col gap-2 overflow-hidden" href={`/news/${newsId}`}>
           <Image
-            className="object-cover xl:object-contain"
+            width={1280}
+            height={853}
             // @todo удалить это фото из public после внедрения цмс
             src="/news/news_2.webp"
-            quality={85}
             alt="Фото учеников школы"
-            fill
           />
-        </div>
-        <div className="flex flex-col gap-2 self-start text-left xl:text-2xl">
-          <p className="text-base font-bold">«Лайк Воллей» выходит на международную арену.</p>
-          <p className="text-[18px] text-foreground-secondary">
-            Едем на «Кубок Дружбы» в Новосибирск!
-          </p>
-        </div>
-        <Link
-          href={`/news/${newsId}`}
-          className="self-start text-xl font-medium text-accent-orange underline underline-offset-4 xl:text-3xl"
-        >
-          Читать
+          <hgroup>
+            <h2 className="text-xl font-medium">Исторический момент для нашей школы!</h2>
+            <p className="text-sm text-pretty">
+              С 20 по 27 апреля 2026 года команда «Лайк Воллей» примет участие в престижных
+              Международных соревнованиях по волейболу «Кубок Дружбы U14» 2026 среди юношей и
+              девушек до 14 лет в г. Новосибирске.
+            </p>
+          </hgroup>
         </Link>
       </section>
-    </article>
+    </section>
   );
 }
