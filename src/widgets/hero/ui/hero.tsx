47c1a1d77aba5d29e078b9d04f03f3ac3ee27cc2
Ivan <ivan.tihii1984@gmail.com>
2026-03-24 14:24:31 +0300

    refactor(widgets): updated the styles for the component design

--- src/widgets/hero/ui/hero.tsx
+++ src/widgets/hero/ui/hero.tsx
@@ -7,9 +7,9 @@ import { MarqueeVertical } from "./marquee-vertical";
 
 export function Hero() {
   return (
-    <section className="relative flex overflow-hidden px-5 pt-10 pb-17.5 text-black md:pt-12.5 xl:px-20">
+    <section className="relative flex overflow-hidden px-5 py-10 text-black md:pt-12.5 xl:px-20 xl:pb-17.5">
       <div className="flex flex-col items-center gap-27.5 md:gap-29.5 md:py-9 md:pr-0 md:pl-9 xl:items-start">
-        <div className="flex flex-col gap-12 text-center md:max-w-180">
+        <div className="flex flex-col gap-12 text-center md:max-w-180 xl:gap-5">
           <Title className="font-inter leading-[40/32] xl:text-left xl:leading-[84/60]">
             Школа волейбола для&nbsp;<span className="text-accent-orange">детей</span>&nbsp;
             <br className="md:hidden" />и<span className="text-accent-blue">&nbsp;взрослыx</span>
@@ -21,12 +21,12 @@ export function Hero() {
           </p>
         </div>
         <SignUpForWorkoutBtn
-          className="z-10 rounded-3xl bg-accent-orange px-3 py-2.5 text-[20px] text-white md:w-86 md:rounded-[40px] md:p-4 md:text-xl"
+          className="z-10 rounded-[40px] bg-accent-orange px-3 py-3.25 text-[20px] text-white md:w-86 md:px-0 md:py-4 md:text-xl"
           label="long"
         />
       </div>
 
-      <div className="absolute top-[34%] -right-[33%] flex origin-top-left rotate-30 xl:top-0 xl:right-0 xl:h-185 xl:w-96 xl:origin-top-right xl:rotate-[16.8deg]">
+      <div className="absolute top-[34%] -right-[33%] flex origin-top-left rotate-30 xl:top-0 xl:right-0 xl:h-185 xl:w-96 xl:origin-top-right xl:rotate-[16.8deg] xl:gap-6">
         <MarqueeVertical className="hidden">
           <Image
             src="/hero/adults_new.webp"
