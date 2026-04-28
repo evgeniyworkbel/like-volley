8a5421a9844801e2e7000b003426c67ee08aef3a
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2026-04-28 10:22:22 +0300

    feat(shared): add migration

--- src/shared/cms/migrations/index.ts
+++ src/shared/cms/migrations/index.ts
@@ -3,6 +3,7 @@ import * as migration_20260217_143602 from "./20260217_143602";
 import * as migration_20260218_174258 from "./20260218_174258";
 import * as migration_20260418_155557 from "./20260418_155557";
 import * as migration_20260419_052606 from "./20260419_052606";
+import * as migration_20260428_072147 from "./20260428_072147";
 
 export const migrations = [
   {
@@ -29,5 +30,10 @@ export const migrations = [
     up: migration_20260419_052606.up,
     down: migration_20260419_052606.down,
     name: "20260419_052606",
+  },
+  {
+    up: migration_20260428_072147.up,
+    down: migration_20260428_072147.down,
+    name: "20260428_072147",
   },
 ];
