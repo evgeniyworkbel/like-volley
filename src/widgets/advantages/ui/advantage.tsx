15073bc427a2a6599fd68596a5d69f24beaad912
Ivan <ivan.tihii1984@gmail.com>
2026-05-06 09:49:58 +0300

    feat(widgets): add the component advantege

---
+++ src/widgets/advantages/ui/advantage.tsx
@@ -0,0 +1,11 @@
+export function Advantage() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4.5 py-10 text-center font-bold xl:justify-between">
+      <h3 className="text-8xl">4.9</h3>
+      <div className="flex flex-col items-center xl:gap-0.5">
+        <h4 className="text-lg xl:text-[28px]">Рейтинг школы</h4>
+        <p className="text-sm text-foreground-secondary">на основе 500+ отзывов</p>
+      </div>
+    </div>
+  );
+}
--- src/widgets/advantages/ui/advantages.tsx
+++ src/widgets/advantages/ui/advantages.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
-import { advantagesData } from "../model/data";
+import { Advantage } from "./advantage";
 
 export function Advantages() {
   return (
-    <section className="mx-auto flex flex-col flex-wrap items-center justify-center gap-12 px-10 py-[74px] text-center text-sm md:max-w-[1026px] md:flex-row xl:max-w-[unset] xl:justify-between xl:px-20">
-      {advantagesData.map(({ id, title, src, description, alt }) => {
-        return (
-          <div key={id} className="flex max-w-[284px] flex-col items-center gap-3 xl:gap-4">
-            <Image src={src} width={80} height={80} alt={alt} />
-            <h3 className="font-bold">{title}</h3>
-            <h4 className="text-balance text-foreground-secondary">{description}</h4>
-          </div>
-        );
-      })}
+    <section className="mx-auto grid grid-cols-1 gap-0 px-7.5 py-5 xl:grid-cols-4 xl:gap-6.5 xl:px-20">
+      <Advantage />
     </section>
   );
 }
