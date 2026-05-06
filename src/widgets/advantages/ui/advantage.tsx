217db418d199fc6066a205a3adc64489df5db888
Ivan <ivan.tihii1984@gmail.com>
2026-05-06 16:16:45 +0300

    refactor(widgets): add span

--- src/widgets/advantages/ui/advantage.tsx
+++ src/widgets/advantages/ui/advantage.tsx
@@ -7,7 +7,7 @@ export function Advantage({ title, subtitle, score, withPlus }: AdvantageProps) 
     <div className="flex flex-col items-center gap-4.5 py-10 font-bold">
       <h3 className="flex text-8xl">
         {score}
-        {withPlus && <p>+</p>}
+        {withPlus && <span>+</span>}
       </h3>
       <div className="flex flex-col items-center gap-0.5 text-center">
         <h4 className="text-lg xl:text-[28px]">{title}</h4>
