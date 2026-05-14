2cd3dacea7e5419ada1f44461cdc65f9da6f1e9f
Ivan <ivan.tihii1984@gmail.com>
2026-05-14 09:36:07 +0300

    fix(widgets): change mobile version contact-form

--- src/widgets/sign-up-for-workout/ui/infinite-ads-line.tsx
+++ src/widgets/sign-up-for-workout/ui/infinite-ads-line.tsx
@@ -3,7 +3,7 @@ import styles from "./infinite-ads-line.module.css";
 
 export function InfiniteAdsLine() {
   return (
-    <div className="flex max-w-255 items-center bg-accent-blue py-0.5">
+    <div className="z-10 flex max-w-255 items-center bg-accent-blue py-0.5">
       <div className={cn("flex gap-6", styles.infiniteLine)}>
         {Array.from({ length: 14 }).map((_, index) => (
           <span key={index} className="font-shantell font-bold text-white">
