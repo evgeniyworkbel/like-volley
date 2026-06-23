59a50da4d255a5a7ecc629ae69c39d7e788d777e
Ivan <ivan.tihii1984@gmail.com>
2026-06-23 05:08:26 +0300

    refactor(app): type checking of the category field has been moved to the page level

--- src/app/(frontend)/(main)/blog/page.tsx
+++ src/app/(frontend)/(main)/blog/page.tsx
@@ -21,7 +21,11 @@ export default async function Blog({ searchParams }: BlogPageProps) {
   });
 
   const postsData = posts.docs;
-  const [mainPost, ...restPosts] = postsData;
+  const mappedPosts = postsData.map((item) => {
+    const category = typeof item.category === "object" ? item.category.label : "";
+    return { ...item, category };
+  });
+  const [mainPost, ...restPosts] = mappedPosts;
 
   return (
     <section className="flex flex-col items-center gap-6 px-5 py-10 md:gap-10 xl:gap-31 xl:px-20 xl:py-12">
@@ -36,20 +40,17 @@ export default async function Blog({ searchParams }: BlogPageProps) {
         createdAt={mainPost.createdAt}
       />
       <section className="grid gap-6 xl:grid-cols-3">
-        {restPosts.map((item) => {
-          const categoryLabel = typeof item.category === "object" ? item.category.label : "";
-          return (
-            <PostMobileCard
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              category={categoryLabel}
-              mainPhoto={item.mainPhoto}
-              readTime={item.readTime}
-              createdAt={item.createdAt}
-            />
-          );
-        })}
+        {restPosts.map((item) => (
+          <PostMobileCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            category={item.category}
+            mainPhoto={item.mainPhoto}
+            readTime={item.readTime}
+            createdAt={item.createdAt}
+          />
+        ))}
       </section>
     </section>
   );
