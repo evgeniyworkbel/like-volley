00181af273bc7a854e397d4bf538e51fd13f9aec
Ivan <ivan.tihii1984@gmail.com>
2026-06-24 11:56:40 +0300

    refactor(entities): change props order

--- src/entities/blog/сopy-link-button.tsx
+++ src/entities/blog/сopy-link-button.tsx
@@ -14,9 +14,9 @@ export function CopyLinkButton({ id }: CopyLinkButtonProps) {
   return (
     <div
       className="flex items-center justify-center rounded-[98px] bg-[oklch(0.9846_0.0017_247.84)] px-3 py-2"
-      onClick={handleCopy}
       role="button"
       tabIndex={0}
+      onClick={handleCopy}
     >
       <LinkIcon />
     </div>
