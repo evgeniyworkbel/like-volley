886b7c87a6f7a0200189632eea96aad3c3ed535b
Ivan <ivan.tihii1984@gmail.com>
2026-06-27 19:50:47 +0300

    fix(app): adjusted the layout according to the design

--- src/app/(frontend)/(main)/blog/[postId]/page.tsx
+++ src/app/(frontend)/(main)/blog/[postId]/page.tsx
@@ -26,27 +26,31 @@ export default async function Post({ params }: PageProps<"/blog/[postId]">) {
   const popularPostsData = mappedPosts;
 
   return (
-    <article className="flex flex-col px-5 py-6 font-inter xl:gap-12 xl:px-20 xl:pt-12 xl:pb-0">
-      <section className="flex flex-col gap-5">
-        <DateWithReadTime date={post.createdAt} readTime={post.readTime} />
-        <div className="flex flex-col gap-8 xl:gap-16">
-          <hgroup className="flex flex-col gap-5">
-            <h1 className="text-5xl leading-11 font-bold wrap-anywhere">{post.title}</h1>
-            <p className="text-lg text-foreground-secondary">{post.shortDescription}</p>
-          </hgroup>
-          <div className="flex flex-col xl:gap-3">
-            <div className="relative flex aspect-[1.6] w-full overflow-hidden rounded-xl xl:aspect-[1.523]">
-              {/* "todo: изменить alt" */}
-              <Image src={post.mainPhoto} alt="Фото учеников школы" fill />
+    <>
+      <article className="flex flex-col px-5 py-6 font-inter xl:gap-12 xl:px-20 xl:py-12">
+        <section className="flex flex-col gap-5">
+          <DateWithReadTime date={post.createdAt} readTime={post.readTime} />
+          <div className="flex flex-col gap-8 xl:gap-16">
+            <hgroup className="flex flex-col gap-5">
+              <h1 className="text-5xl leading-11 font-bold wrap-anywhere">{post.title}</h1>
+              <p className="text-lg text-foreground-secondary">{post.shortDescription}</p>
+            </hgroup>
+            <div className="flex flex-col xl:gap-3">
+              <div className="relative flex aspect-[1.6] w-full overflow-hidden rounded-xl xl:aspect-[1.523]">
+                {/* "todo: изменить alt" */}
+                <Image src={post.mainPhoto} alt="Фото учеников школы" fill />
+              </div>
+              <p className="text-right text-base text-foreground-secondary">
+                Фото сделано: {post.mainPhotoMadeBy}
+              </p>
             </div>
-            <p className="text-right text-base text-foreground-secondary">
-              Фото сделано: {post.mainPhotoMadeBy}
-            </p>
+            <div
+              dangerouslySetInnerHTML={{ __html: convertLexicalToHTML({ data: post.content }) }}
+            />
           </div>
-          <div dangerouslySetInnerHTML={{ __html: convertLexicalToHTML({ data: post.content }) }} />
-        </div>
-      </section>
+        </section>
+      </article>
       <PopularPosts popularPosts={popularPostsData} />
-    </article>
+    </>
   );
 }
