d38c2d54d1bd4e016c8db583e3e26a468c1427ab
Ivan <ivan.tihii1984@gmail.com>
2026-07-03 15:22:44 +0300

    refactor(app): remove fill-current, forArrowTopIcon

--- src/shared/ui/nav-arrow.tsx
+++ src/shared/ui/nav-arrow.tsx
@@ -3,7 +3,7 @@ import { ArrowTopIcon } from "../icons";
 export function NavArrow() {
   return (
     <span className="flex size-8.5 items-center justify-center rounded-full border border-accent-orange">
-      <ArrowTopIcon className="rotate-90 fill-current text-accent-orange" />
+      <ArrowTopIcon className="rotate-90 text-accent-orange" />
     </span>
   );
 }
