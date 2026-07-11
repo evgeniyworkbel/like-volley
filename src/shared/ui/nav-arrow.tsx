6adef12b1bc6d10beead2a6e2342e3b6c09aeccc
Ivan <ivan.tihii1984@gmail.com>
2026-07-11 18:32:17 +0300

    refactor(shared): changed size NavArrow to 48px

--- src/shared/ui/nav-arrow.tsx
+++ src/shared/ui/nav-arrow.tsx
@@ -2,7 +2,7 @@ import { ArrowTopIcon } from "../icons";
 
 export function NavArrow() {
   return (
-    <span className="flex size-8.5 items-center justify-center rounded-full border border-accent-orange">
+    <span className="flex size-12 items-center justify-center rounded-full border border-accent-orange">
       <ArrowTopIcon className="rotate-90 text-accent-orange" />
     </span>
   );
