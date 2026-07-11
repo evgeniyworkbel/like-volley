31fba7d1837ed16eafc6b4970880356b28eb3614
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2026-07-11 18:32:46 +0300

    fix(features): safe get href in server environment and safe call navigator api during copying link

--- src/features/share-post/ui/share-post.tsx
+++ src/features/share-post/ui/share-post.tsx
@@ -18,34 +18,36 @@ import { toast } from "sonner";
 
 type SharePostProps = { className?: string; url?: string };
 
-export function SharePost({ className, url = window.location.href }: SharePostProps) {
-  const handleCopyLink = () => {
-    navigator.clipboard
-      .writeText(url)
-      .then(() => toast.success("Ссылка успешно скопирована"))
-      .catch((err) => {
-        toast.error("Произошла ошибка при копировании ссылки");
-        console.error("Failed to copy link: ", err);
-      });
+export function SharePost({ className, url }: SharePostProps) {
+  const href = typeof window === "undefined" ? "" : url || window.location.href;
+
+  const handleCopyLink = async () => {
+    try {
+      await navigator.clipboard.writeText(href);
+      toast.success("Ссылка успешно скопирована");
+    } catch (err) {
+      toast.error("Не удалось скопировать ссылку");
+      console.error("Failed to copy link: ", err);
+    }
   };
 
   return (
     <section
       className={cn("flex items-center gap-2.5 rounded-full bg-[#F9FAFB] px-3 py-2", className)}
     >
-      <FacebookShareButton url={url} aria-label="Поделиться этой ссылкой в Facebook">
+      <FacebookShareButton url={href} aria-label="Поделиться этой ссылкой в Facebook">
         <FacebookIcon size={24} round />
       </FacebookShareButton>
-      <TelegramShareButton url={url} aria-label="Поделиться этой ссылкой в Telegram">
+      <TelegramShareButton url={href} aria-label="Поделиться этой ссылкой в Telegram">
         <TelegramIcon size={24} round />
       </TelegramShareButton>
-      <VKShareButton url={url} aria-label="Поделиться этой ссылкой в ВКонтакте">
+      <VKShareButton url={href} aria-label="Поделиться этой ссылкой в ВКонтакте">
         <VKIcon size={24} round />
       </VKShareButton>
-      <OKShareButton url={url} aria-label="Поделиться этой ссылкой в Одноклассники">
+      <OKShareButton url={href} aria-label="Поделиться этой ссылкой в Одноклассники">
         <OKIcon size={24} round />
       </OKShareButton>
-      <ThreadsShareButton url={url} aria-label="Поделиться этой ссылкой в Threads">
+      <ThreadsShareButton url={href} aria-label="Поделиться этой ссылкой в Threads">
         <ThreadsIcon size={24} round />
       </ThreadsShareButton>
       <span className="h-6 w-0.5 shrink-0 bg-[#CACACA]" />
