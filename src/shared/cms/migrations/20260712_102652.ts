37ce98d892c94eff0c2ba6458a68e0149806fbe9
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2026-07-12 13:31:09 +0300

    feat(shared): change type of mainPhoto column, create migration

---
+++ src/shared/cms/migrations/20260712_102652.ts
@@ -0,0 +1,17 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from "@payloadcms/db-postgres";
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "posts" RENAME COLUMN "main_photo" TO "main_photo_id";
+  ALTER TABLE "posts" ADD CONSTRAINT "posts_main_photo_id_media_id_fk" FOREIGN KEY ("main_photo_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
+  CREATE INDEX "posts_main_photo_idx" ON "posts" USING btree ("main_photo_id");`);
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "posts" DROP CONSTRAINT "posts_main_photo_id_media_id_fk";
+  
+  DROP INDEX "posts_main_photo_idx";
+  ALTER TABLE "posts" ADD COLUMN "main_photo" varchar NOT NULL;
+  ALTER TABLE "posts" DROP COLUMN "main_photo_id";`);
+}
