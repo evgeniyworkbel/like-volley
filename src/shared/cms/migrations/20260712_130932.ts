a28323d0e37024dd7d01462840776d261718e20c
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2026-07-12 16:25:03 +0300

    fix(shared): update type of column

--- src/shared/cms/migrations/20260712_130932.ts
+++ src/shared/cms/migrations/20260712_130932.ts
@@ -3,6 +3,7 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from "@payloadcms/db-postgres";
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
    ALTER TABLE "posts" RENAME COLUMN "main_photo" TO "main_photo_id";
+   ALTER TABLE "posts" ALTER COLUMN "main_photo_id" TYPE integer USING (main_photo_id::integer);
   ALTER TABLE "posts" ADD CONSTRAINT "posts_main_photo_id_media_id_fk" FOREIGN KEY ("main_photo_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
   CREATE INDEX "posts_main_photo_idx" ON "posts" USING btree ("main_photo_id");`);
 }
