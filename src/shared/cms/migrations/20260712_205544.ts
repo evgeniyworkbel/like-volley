3b534f98117e5aea2d02f25ecdd38464892af066
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2026-07-13 00:30:29 +0300

    fix(shared): set default for photo_id column in owner table

--- src/shared/cms/migrations/20260712_205544.ts
+++ src/shared/cms/migrations/20260712_205544.ts
@@ -2,7 +2,7 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from "@payloadcms/db-postgres";
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "owner" ADD COLUMN "photo_id" integer NOT NULL;
+   ALTER TABLE "owner" ADD COLUMN "photo_id" integer NOT NULL DEFAULT 1;
   ALTER TABLE "owner" ADD CONSTRAINT "owner_photo_id_media_id_fk" FOREIGN KEY ("photo_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
   CREATE INDEX "owner_photo_idx" ON "owner" USING btree ("photo_id");`);
 }
