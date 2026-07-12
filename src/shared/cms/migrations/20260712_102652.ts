ce8c358f3ddde645d6bf84de801fa2542fd9b415
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2026-07-12 15:01:37 +0300

    fix(shared): try to resolve db migration error on vercel

--- src/shared/cms/migrations/20260712_102652.ts
+++ src/shared/cms/migrations/20260712_102652.ts
@@ -1,18 +1,22 @@
 import { MigrateUpArgs, MigrateDownArgs, sql } from "@payloadcms/db-postgres";
 
-export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+export async function up({ db }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   UPDATE posts SET main_photo_id = NULL WHERE main_photo_id IS NOT NULL AND NOT EXISTS (SELECT 1 FROM media WHERE id = main_photo_id);
-  ALTER TABLE "posts" RENAME COLUMN "main_photo" TO "main_photo_id";
-  ALTER TABLE "posts" ADD CONSTRAINT "posts_main_photo_id_media_id_fk" FOREIGN KEY ("main_photo_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
-  CREATE INDEX "posts_main_photo_idx" ON "posts" USING btree ("main_photo_id");`);
+    ALTER TABLE "posts" RENAME COLUMN "main_photo" TO "main_photo_id";
+    ALTER TABLE "posts" ALTER COLUMN "main_photo_id" DROP NOT NULL;
+    UPDATE "posts" SET "main_photo_id" = NULL;
+    ALTER TABLE "posts" ALTER COLUMN "main_photo_id" TYPE integer USING NULL;
+    ALTER TABLE "posts" ADD CONSTRAINT "posts_main_photo_id_media_id_fk"
+      FOREIGN KEY ("main_photo_id") REFERENCES "public"."media"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+    CREATE INDEX "posts_main_photo_idx" ON "posts" USING btree ("main_photo_id");
+  `);
 }
 
-export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+export async function down({ db }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "posts" DROP CONSTRAINT "posts_main_photo_id_media_id_fk";
-  
-  DROP INDEX "posts_main_photo_idx";
-  ALTER TABLE "posts" ADD COLUMN "main_photo" varchar NOT NULL;
-  ALTER TABLE "posts" DROP COLUMN "main_photo_id";`);
+    ALTER TABLE "posts" DROP CONSTRAINT "posts_main_photo_id_media_id_fk";
+    DROP INDEX "posts_main_photo_idx";
+    ALTER TABLE "posts" ALTER COLUMN "main_photo_id" TYPE varchar USING "main_photo_id"::varchar;
+    ALTER TABLE "posts" RENAME COLUMN "main_photo_id" TO "main_photo";
+  `);
 }
