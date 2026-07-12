76d3b1fdc3195b2b32112b7c2b12e307c0780254
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2026-07-12 14:13:39 +0300

    fix(shared): try to resolve db migration error on vercel

--- src/shared/cms/migrations/20260712_102652.ts
+++ src/shared/cms/migrations/20260712_102652.ts
@@ -2,7 +2,8 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from "@payloadcms/db-postgres";
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "posts" RENAME COLUMN "main_photo" TO "main_photo_id";
+   UPDATE posts SET main_photo_id = NULL WHERE main_photo_id IS NOT NULL AND NOT EXISTS (SELECT 1 FROM media WHERE id = main_photo_id);
+  ALTER TABLE "posts" RENAME COLUMN "main_photo" TO "main_photo_id";
   ALTER TABLE "posts" ADD CONSTRAINT "posts_main_photo_id_media_id_fk" FOREIGN KEY ("main_photo_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
   CREATE INDEX "posts_main_photo_idx" ON "posts" USING btree ("main_photo_id");`);
 }
