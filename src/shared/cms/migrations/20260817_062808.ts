54546c36e8c3c54ce3ca3c5688c917cf44b1d940
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2026-08-22 23:55:23 +0300

    fix(shared): add default for new "link" column of photoalbums collection

--- src/shared/cms/migrations/20260817_062808.ts
+++ src/shared/cms/migrations/20260817_062808.ts
@@ -2,7 +2,7 @@ import { MigrateUpArgs, MigrateDownArgs, sql } from "@payloadcms/db-postgres";
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "photo_albums" ADD COLUMN "link" varchar NOT NULL;`);
+   ALTER TABLE "photo_albums" ADD COLUMN "link" varchar NOT NULL DEFAULT 'https://zaviflash.wfolio.pro/disk/29-11-2024-like-volley-2-8zmnl7';`);
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
